88c75c32a80acf3af2d77f1756c9b5b555781a17
Philippe Daouadi <philippe.daouadi@tanker.io>
2019-01-30 09:57:01 +0100

    fix(emscripten): fix unresolved symbol

--- modules/sdk-core/include/Tanker/Emscripten/Helpers.hpp
+++ modules/sdk-core/include/Tanker/Emscripten/Helpers.hpp
@@ -12,12 +12,24 @@ namespace Tanker
 namespace Emscripten
 {
 template <typename T>
-emscripten::val containerToJs(T const& vec);
+emscripten::val containerToJs(T const& cont)
+{
+  using emscripten::val;
+
+  auto const Uint8Array = val::global("Uint8Array");
+  val memory = val::module_property("buffer");
+  return Uint8Array.new_(
+      memory, reinterpret_cast<uintptr_t>(cont.data()), cont.size());
+}
 
 std::vector<uint8_t> copyToVector(const emscripten::val& typedArray);
 
 template <typename Sig>
-emscripten::val toJsFunctionObject(std::function<Sig> functor);
+emscripten::val toJsFunctionObject(std::function<Sig> functor)
+{
+  return emscripten::val(functor)["opcall"].template call<emscripten::val>(
+      "bind", emscripten::val(functor));
+}
 
 tc::cotask<emscripten::val> jsPromiseToFuture(emscripten::val const& jspromise);
 }
