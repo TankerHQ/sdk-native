48593e18224f099586258947acd951edcd71d3df
Loic Banet <loic.banet@tanker.io>
2019-02-07 18:52:55 +0100

    fix(encryption-format): update format

--- modules/sdk-core/include/Tanker/EncryptionFormat/EncryptorV4.hpp
+++ modules/sdk-core/include/Tanker/EncryptionFormat/EncryptorV4.hpp
@@ -14,18 +14,30 @@ namespace EncryptionFormat
 {
 namespace EncryptorV4
 {
+namespace Default
+{
+constexpr uint32_t encryptedChunkSize = 1024lu * 1024lu;
+}
 constexpr uint32_t version()
 {
   return 4u;
 }
 
-uint64_t encryptedSize(uint64_t clearSize);
+uint64_t encryptedSize(
+    uint64_t clearSize,
+    uint32_t encryptedChunkSize = Default::encryptedChunkSize);
+
 uint64_t decryptedSize(gsl::span<uint8_t const> encryptedData);
+
 EncryptionFormat::EncryptionMetadata encrypt(
-    uint8_t* encryptedData, gsl::span<uint8_t const> clearData);
+    uint8_t* encryptedData,
+    gsl::span<uint8_t const> clearData,
+    uint32_t encryptedChunkSize = Default::encryptedChunkSize);
+
 void decrypt(uint8_t* decryptedData,
              Crypto::SymmetricKey const& key,
              gsl::span<uint8_t const> encryptedData);
+
 ResourceId extractResourceId(gsl::span<uint8_t const> encryptedData);
 }
 }
--- modules/sdk-core/src/EncryptionFormat/EncryptorV4.cpp
+++ modules/sdk-core/src/EncryptionFormat/EncryptorV4.cpp
@@ -15,38 +15,40 @@ namespace EncryptorV4
 {
 namespace
 {
+constexpr auto sizeOfChunkSize = sizeof(uint32_t);
 auto const versionSize = Serialization::varint_size(version());
-constexpr auto encryptedChunkSize = 1024lu * 1024lu;
-auto const clearChunkSize =
-    Crypto::decryptedSize(encryptedChunkSize - Crypto::AeadIv::arraySize);
-auto const sizeOfEncryptedChunkSize =
-    Serialization::varint_size(encryptedChunkSize);
+auto const headerSize = versionSize + sizeOfChunkSize + ResourceId::arraySize;
+
+uint32_t clearChunkSize(uint32_t const encryptedChunkSize)
+{
+  return encryptedChunkSize - headerSize - Crypto::AeadIv::arraySize -
+         Crypto::Mac::arraySize;
+}
 
 // version 4 format layout:
-// [version, 1B] [RESOURCEID, 16B] +
-// N * [[IV, 24B] [[ciphertext, variable] [MAC, 16B]]]
+// N * chunk of encryptedChunkSize:
+// header: [version, 1B] [chunkSize, 4B] [ResourceId, 16B]
+// content: [IV seed, 24B] [ciphertext, variable] [MAC, 16B]
 void checkEncryptedFormat(gsl::span<uint8_t const> encryptedData)
 {
   auto const dataVersionResult = Serialization::varint_read(encryptedData);
-  auto const chunkSizeResult =
-      Serialization::varint_read(dataVersionResult.second);
-  auto const overheadSize = ResourceId::arraySize + Crypto::AeadIv::arraySize +
-                            Crypto::Mac::arraySize;
+  auto const overheadSize = sizeOfChunkSize + ResourceId::arraySize +
+                            Crypto::AeadIv::arraySize + Crypto::Mac::arraySize;
+
   assert(dataVersionResult.first == version());
 
-  if (chunkSizeResult.second.size() < overheadSize)
+  if (dataVersionResult.second.size() < overheadSize)
     throw Error::DecryptFailed("truncated encrypted buffer");
 }
 }
 
-uint64_t encryptedSize(uint64_t clearSize)
+uint64_t encryptedSize(uint64_t clearSize, uint32_t encryptedChunkSize)
 {
-  auto const chunks = clearSize / clearChunkSize;
-  auto const lastclearChunkSize = clearSize % clearChunkSize;
-  auto const headerSize =
-      versionSize + ResourceId::arraySize + sizeOfEncryptedChunkSize;
-  return headerSize + chunks * encryptedChunkSize +
-         Crypto::encryptedSize(lastclearChunkSize) + Crypto::AeadIv::arraySize;
+  auto const chunkSize = clearChunkSize(encryptedChunkSize);
+  auto const chunks = clearSize / chunkSize;
+  auto const lastClearChunkSize = clearSize % chunkSize;
+  return chunks * encryptedChunkSize + headerSize + Crypto::AeadIv::arraySize +
+         Crypto::encryptedSize(lastClearChunkSize);
 }
 
 uint64_t decryptedSize(gsl::span<uint8_t const> encryptedData)
@@ -56,16 +58,16 @@ uint64_t decryptedSize(gsl::span<uint8_t const> encryptedData)
     checkEncryptedFormat(encryptedData);
 
     auto const versionResult = Serialization::varint_read(encryptedData);
-    auto const chunkSizeResult =
-        Serialization::varint_read(versionResult.second);
-    if (chunkSizeResult.second.size() < ResourceId::arraySize)
+    auto const encryptedChunkSize = Serialization::deserialize<uint32_t>(
+        versionResult.second.subspan(0, sizeOfChunkSize));
+    if (versionResult.second.size() < sizeOfChunkSize + ResourceId::arraySize)
       throw Error::DecryptFailed("truncated encrypted buffer");
-    auto const encryptedDataSize =
-        chunkSizeResult.second.size() - ResourceId::arraySize;
-    auto const chunks = encryptedDataSize / chunkSizeResult.first;
-    auto const lastclearChunkSize =
-        (encryptedDataSize % chunkSizeResult.first) - Crypto::AeadIv::arraySize;
-    return chunks * clearChunkSize + Crypto::decryptedSize(lastclearChunkSize);
+    auto const chunks = encryptedData.size() / encryptedChunkSize;
+    auto const lastClearChunkSize =
+        (encryptedData.size() % encryptedChunkSize) -
+        Crypto::AeadIv::arraySize - headerSize;
+    return chunks * clearChunkSize(encryptedChunkSize) +
+           Crypto::decryptedSize(lastClearChunkSize);
   }
   catch (std::out_of_range const&)
   {
@@ -74,27 +76,30 @@ uint64_t decryptedSize(gsl::span<uint8_t const> encryptedData)
 }
 
 EncryptionFormat::EncryptionMetadata encrypt(uint8_t* encryptedData,
-                                             gsl::span<uint8_t const> clearData)
+                                             gsl::span<uint8_t const> clearData,
+                                             uint32_t encryptedChunkSize)
 {
-  Serialization::varint_write(encryptedData, version());
-  Serialization::varint_write(encryptedData + versionSize, encryptedChunkSize);
+  auto const chunkSize = clearChunkSize(encryptedChunkSize);
   auto const key = Crypto::makeSymmetricKey();
-  auto const resourceId =
-      gsl::span<uint8_t>(encryptedData + versionSize + sizeOfEncryptedChunkSize,
-                         ResourceId::arraySize);
+  ResourceId resourceId{};
   Crypto::randomFill(resourceId);
-  auto chunk = encryptedData + ResourceId::arraySize + versionSize +
-               sizeOfEncryptedChunkSize;
 
   for (uint64_t clearDataIndex = 0; clearDataIndex <= clearData.size();
-       chunk += encryptedChunkSize, clearDataIndex += clearChunkSize)
+       encryptedData += encryptedChunkSize, clearDataIndex += chunkSize)
   {
+    // write header
+    Serialization::varint_write(encryptedData, version());
+    Serialization::serialize(encryptedData + versionSize, encryptedChunkSize);
+    Serialization::serialize(encryptedData + versionSize + sizeOfChunkSize,
+                             resourceId);
+    auto chunk = encryptedData + headerSize;
+
     auto const ivSeed = gsl::span<uint8_t>(chunk, Crypto::AeadIv::arraySize);
     Crypto::randomFill(ivSeed);
-    auto const numericalIndex = clearDataIndex / clearChunkSize;
+    auto const numericalIndex = clearDataIndex / chunkSize;
     auto const iv = Crypto::deriveIv(Crypto::AeadIv{ivSeed}, numericalIndex);
-    auto const nextClearChunkSize = std::min<unsigned long>(
-        clearChunkSize, clearData.size() - clearDataIndex);
+    auto const nextClearChunkSize =
+        std::min<unsigned long>(chunkSize, clearData.size() - clearDataIndex);
 
     Crypto::encryptAead(key,
                         iv.data(),
@@ -114,31 +119,36 @@ void decrypt(uint8_t* decryptedData,
   {
     checkEncryptedFormat(encryptedData);
 
-    auto const versionRemoved =
-        Serialization::varint_read(encryptedData).second;
-    auto const chunkSizePair = Serialization::varint_read(versionRemoved);
-    auto const chunkSize = chunkSizePair.first;
-    auto const cipherText = chunkSizePair.second;
-    auto const chunks = cipherText.subspan(
-        ResourceId::arraySize, cipherText.size() - ResourceId::arraySize);
+    auto const versionResult = Serialization::varint_read(encryptedData);
+    auto const encryptedChunkSize = Serialization::deserialize<uint32_t>(
+        versionResult.second.subspan(0, sizeOfChunkSize));
 
-    for (uint64_t currentSize = 0; currentSize < chunks.size();
-         currentSize += chunkSize)
+    for (uint64_t currentSize = 0; currentSize < encryptedData.size();
+         currentSize += encryptedChunkSize)
     {
-      auto const numericalIndex = currentSize / chunkSize;
-      auto const nextclearChunkSize =
-          std::min<unsigned long>(chunkSize, chunks.size() - currentSize);
-      auto const chunk = chunks.subspan(currentSize, nextclearChunkSize);
+      auto const numericalIndex = currentSize / encryptedChunkSize;
+      auto const nextClearChunkSize = std::min<unsigned long>(
+          encryptedChunkSize, encryptedData.size() - currentSize);
+      auto const chunk = encryptedData.subspan(currentSize, nextClearChunkSize);
+
+      // Header
+      auto const versionResult = Serialization::varint_read(chunk);
+      if (versionResult.first != version())
+        throw Error::formatEx<Error::VersionNotSupported>(
+            "unsupported version: {:d}", versionResult.first);
+      auto const headerRemoved =
+          versionResult.second.subspan(ResourceId::arraySize + sizeOfChunkSize);
+
       auto const ivSeed =
-          Crypto::AeadIv{chunk.subspan(0, Crypto::AeadIv::arraySize)};
+          Crypto::AeadIv{headerRemoved.subspan(0, Crypto::AeadIv::arraySize)};
       auto const iv = Crypto::deriveIv(ivSeed, numericalIndex);
-      auto const cipherText = chunk.subspan(
-          Crypto::AeadIv::arraySize, chunk.size() - Crypto::AeadIv::arraySize);
-      Crypto::decryptAead(key,
-                          iv.data(),
-                          decryptedData + numericalIndex * clearChunkSize,
-                          cipherText,
-                          {});
+      auto const cipherText = headerRemoved.subspan(Crypto::AeadIv::arraySize);
+      Crypto::decryptAead(
+          key,
+          iv.data(),
+          decryptedData + numericalIndex * clearChunkSize(encryptedChunkSize),
+          cipherText,
+          {});
     }
   }
   catch (std::out_of_range const&)
@@ -158,9 +168,8 @@ ResourceId extractResourceId(gsl::span<uint8_t const> encryptedData)
     checkEncryptedFormat(encryptedData);
 
     auto const versionPair = Serialization::varint_read(encryptedData);
-    auto const cipherText =
-        Serialization::varint_read(versionPair.second).second;
-    auto const mac = cipherText.subspan(0, ResourceId::arraySize);
+    auto const mac =
+        versionPair.second.subspan(sizeof(uint32_t), ResourceId::arraySize);
     return ResourceId{mac};
   }
   catch (std::out_of_range const&)
