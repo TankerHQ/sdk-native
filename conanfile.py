357cc1152b2e2896a7b3b6e7d314bdb847b21e84
Théo DELRIEU <theo.delrieu@tanker.io>
2019-04-02 12:13:40 +0200

    use tconcurrent 0.18.2

--- conanfile.py
+++ conanfile.py
@@ -63,7 +63,7 @@ class TankerConan(ConanFile):
         self.requires("libsodium/1.0.16@tanker/testing", private=private)
         self.requires("mockaron/0.9.0@tanker/stable", private=private)
         self.requires("optional-lite/3.1.1@tanker/testing", private=private)
-        self.requires("tconcurrent/0.18.1@tanker/stable", private=private)
+        self.requires("tconcurrent/0.18.2@tanker/stable", private=private)
         self.requires("variant/1.3.0@tanker/testing", private=private)
 
     def imports(self):
