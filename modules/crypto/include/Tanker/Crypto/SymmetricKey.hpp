b5db24e29751b0ea27076290b26b63c0bbfd68bb
Théo DELRIEU <theo.delrieu@tanker.io>
2019-04-10 16:34:00 +0200

    crypto: make SymmetricKey derive from BasicCryptographicType

--- modules/crypto/include/Tanker/Crypto/SymmetricKey.hpp
+++ modules/crypto/include/Tanker/Crypto/SymmetricKey.hpp
@@ -1,22 +1,37 @@
 #pragma once
 
-#include <Tanker/Crypto/detail/CryptographicType.hpp>
-#include <Tanker/Crypto/detail/IsCryptographicType.hpp>
-#include <Tanker/Crypto/detail/ArrayHelpers.hpp>
+#include <Tanker/Crypto/BasicCryptographicType.hpp>
 
 #include <sodium/crypto_aead_xchacha20poly1305.h>
+
+#include <tuple>
+#include <type_traits>
 
 namespace Tanker
 {
 namespace Crypto
 {
-TANKER_CRYPTO_CRYPTOGRAPHIC_TYPE(SymmetricKey,
-                                 crypto_aead_xchacha20poly1305_ietf_KEYBYTES)
-TANKER_CRYPTO_IS_CRYPTOGRAPHIC_TYPE(SymmetricKey)
+class SymmetricKey
+  : public BasicCryptographicType<SymmetricKey,
+                                  crypto_aead_xchacha20poly1305_ietf_KEYBYTES>
+{
+  using base_t::base_t;
+};
 }
 }
 
 namespace std
 {
-TANKER_CRYPTO_STD_TUPLE_SIZE_ELEMENT(::Tanker::Crypto::SymmetricKey)
+template <>
+class tuple_size<::Tanker::Crypto::SymmetricKey>
+  : public integral_constant<size_t,
+                             crypto_aead_xchacha20poly1305_ietf_KEYBYTES>
+{
+};
+
+template <size_t I>
+class tuple_element<I, ::Tanker::Crypto::SymmetricKey>
+  : public tuple_element<I, ::Tanker::Crypto::SymmetricKey::base_t>
+{
+};
 }
