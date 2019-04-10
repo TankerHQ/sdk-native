1dde91a31ad93c1fbef272b310e7451b50a62aaa
Théo DELRIEU <theo.delrieu@tanker.io>
2019-04-10 16:52:14 +0200

    crypto: make PrivateEncryptionKey derive from BasicCryptographicType

--- modules/crypto/include/Tanker/Crypto/PrivateEncryptionKey.hpp
+++ modules/crypto/include/Tanker/Crypto/PrivateEncryptionKey.hpp
@@ -1,17 +1,11 @@
 #pragma once
 
 #include <Tanker/Crypto/AsymmetricKey.hpp>
-#include <Tanker/Crypto/IsCryptographicType.hpp>
+#include <Tanker/Crypto/BasicCryptographicType.hpp>
 #include <Tanker/Crypto/KeyType.hpp>
 #include <Tanker/Crypto/KeyUsage.hpp>
 
-#include <Tanker/Crypto/detail/CryptographicTypeImpl.hpp>
-
 #include <sodium/crypto_box.h>
-
-#include <array>
-#include <cstdint>
-#include <type_traits>
 
 namespace Tanker
 {
@@ -19,19 +13,14 @@ namespace Crypto
 {
 template <>
 class AsymmetricKey<KeyType::Private, KeyUsage::Encryption>
-  : std::array<std::uint8_t, crypto_box_SECRETKEYBYTES>
+  : public BasicCryptographicType<
+        AsymmetricKey<KeyType::Private, KeyUsage::Encryption>,
+        crypto_box_SECRETKEYBYTES>
 {
-  TANKER_CRYPTO_CRYPTOGRAPHIC_TYPE_IMPL(AsymmetricKey,
-                                        crypto_box_SECRETKEYBYTES,
-                                        PrivateEncryptionKey)
+  using base_t::base_t;
 };
 
 using PrivateEncryptionKey =
     AsymmetricKey<KeyType::Private, KeyUsage::Encryption>;
-
-template <>
-struct IsCryptographicType<PrivateEncryptionKey> : std::true_type
-{
-};
 }
 }
