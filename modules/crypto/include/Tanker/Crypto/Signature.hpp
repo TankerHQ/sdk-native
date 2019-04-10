f3aa3b73c7f3223a407c2ba79895a35144eaeffe
Théo DELRIEU <theo.delrieu@tanker.io>
2019-04-10 16:32:01 +0200

    crypto: make Signature derive from BasicCryptographicType

--- modules/crypto/include/Tanker/Crypto/Signature.hpp
+++ modules/crypto/include/Tanker/Crypto/Signature.hpp
@@ -1,21 +1,34 @@
 #pragma once
 
-#include <Tanker/Crypto/detail/CryptographicType.hpp>
-#include <Tanker/Crypto/detail/IsCryptographicType.hpp>
-#include <Tanker/Crypto/detail/ArrayHelpers.hpp>
+#include <Tanker/Crypto/BasicCryptographicType.hpp>
 
 #include <sodium/crypto_sign.h>
+
+#include <tuple>
+#include <type_traits>
 
 namespace Tanker
 {
 namespace Crypto
 {
-TANKER_CRYPTO_CRYPTOGRAPHIC_TYPE(Signature, crypto_sign_BYTES)
-TANKER_CRYPTO_IS_CRYPTOGRAPHIC_TYPE(Signature)
+class Signature : public BasicCryptographicType<Signature, crypto_sign_BYTES>
+{
+  using base_t::base_t;
+};
 }
 }
 
 namespace std
 {
-TANKER_CRYPTO_STD_TUPLE_SIZE_ELEMENT(::Tanker::Crypto::Signature)
+template <>
+class tuple_size<::Tanker::Crypto::Signature>
+  : public integral_constant<size_t, crypto_sign_BYTES>
+{
+};
+
+template <size_t I>
+class tuple_element<I, ::Tanker::Crypto::Signature>
+  : public tuple_element<I, ::Tanker::Crypto::Signature::base_t>
+{
+};
 }
