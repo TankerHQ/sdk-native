ede80bf4b9b866edcde49d656b2b4676049c280a
Théo DELRIEU <theo.delrieu@tanker.io>
2019-04-10 15:25:14 +0200

    crypto: make PublicSignatureKey derive from BasicCryptographicType

--- modules/crypto/include/Tanker/Crypto/PublicSignatureKey.hpp
+++ modules/crypto/include/Tanker/Crypto/PublicSignatureKey.hpp
@@ -1,17 +1,11 @@
 #pragma once
 
 #include <Tanker/Crypto/AsymmetricKey.hpp>
-#include <Tanker/Crypto/IsCryptographicType.hpp>
+#include <Tanker/Crypto/BasicCryptographicType.hpp>
 #include <Tanker/Crypto/KeyType.hpp>
 #include <Tanker/Crypto/KeyUsage.hpp>
 
-#include <Tanker/Crypto/detail/CryptographicTypeImpl.hpp>
-
 #include <sodium/crypto_sign.h>
-
-#include <array>
-#include <cstdint>
-#include <type_traits>
 
 namespace Tanker
 {
@@ -19,19 +13,14 @@ namespace Crypto
 {
 template <>
 class AsymmetricKey<KeyType::Public, KeyUsage::Signature>
-  : std::array<std::uint8_t, crypto_sign_PUBLICKEYBYTES>
+  : public BasicCryptographicType<
+        AsymmetricKey<KeyType::Public, KeyUsage::Signature>,
+        crypto_sign_PUBLICKEYBYTES>
 {
-  TANKER_CRYPTO_CRYPTOGRAPHIC_TYPE_IMPL(AsymmetricKey,
-                                        crypto_sign_PUBLICKEYBYTES,
-                                        PublicSignatureKey)
+  using base_t::base_t;
 };
 
 using PublicSignatureKey =
     AsymmetricKey<KeyType::Public, KeyUsage::Signature>;
-
-template <>
-struct IsCryptographicType<PublicSignatureKey> : std::true_type
-{
-};
 }
 }
