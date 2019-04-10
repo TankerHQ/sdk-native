2cb2e933af379083538a9d1b72c3ed8923b4a2bf
Théo DELRIEU <theo.delrieu@tanker.io>
2019-04-10 14:46:15 +0200

    crypto: add SFINAE template parameter to IsCryptographicType

--- modules/crypto/include/Tanker/Crypto/IsCryptographicType.hpp
+++ modules/crypto/include/Tanker/Crypto/IsCryptographicType.hpp
@@ -1,10 +1,14 @@
 #pragma once
 
+#include <type_traits>
+
 namespace Tanker
 {
 namespace Crypto
 {
-template <typename T>
-struct IsCryptographicType;
+template <typename T, typename = void>
+struct IsCryptographicType : std::false_type
+{
+};
 }
 }
