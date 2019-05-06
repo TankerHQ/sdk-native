c1030b9b2fba3c2fce6690845d547e6c7493d933
Philippe Daouadi <philippe.daouadi@tanker.io>
2019-05-06 16:32:53 +0200

    Trustchain: simplify implementation macros

--- modules/trustchain/include/Tanker/Trustchain/Preprocessor/Actions/Implementation.hpp
+++ modules/trustchain/include/Tanker/Trustchain/Preprocessor/Actions/Implementation.hpp
@@ -2,48 +2,43 @@
 
 #include <Tanker/Trustchain/Preprocessor/detail/Common.hpp>
 
-#include <boost/preprocessor/arithmetic/add.hpp>
-#include <boost/preprocessor/comparison/less.hpp>
 #include <boost/preprocessor/empty.hpp>
 #include <boost/preprocessor/punctuation/comma_if.hpp>
 #include <boost/preprocessor/seq/for_each.hpp>
 #include <boost/preprocessor/seq/for_each_i.hpp>
-#include <boost/preprocessor/seq/size.hpp>
 #include <boost/preprocessor/variadic/to_seq.hpp>
 
 #include <tuple>
 
-#define TANKER_DETAIL_CONSTRUCTOR_ARGS(unused, size, idx, elem) \
-  TANKER_DETAIL_FULL_PARAMETER(elem)                            \
-  BOOST_PP_COMMA_IF(BOOST_PP_LESS(BOOST_PP_ADD(idx, 1), size))
+#define TANKER_DETAIL_CONSTRUCTOR_ARGS(unused1, unused2, idx, elem) \
+  BOOST_PP_COMMA_IF(idx)                                            \
+  TANKER_DETAIL_FULL_PARAMETER(elem)
 
-#define TANKER_DETAIL_CONSTRUCTOR_INIT_LIST(unused, size, idx, elem) \
-  TANKER_DETAIL_ATTRIBUTE_NAME(elem)                                 \
-  (TANKER_DETAIL_PARAMETER_NAME(elem))                               \
-      BOOST_PP_COMMA_IF(BOOST_PP_LESS(BOOST_PP_ADD(idx, 1), size))
+#define TANKER_DETAIL_CONSTRUCTOR_INIT_LIST(unused1, unused2, idx, elem) \
+  BOOST_PP_COMMA_IF(idx)                                                 \
+  TANKER_DETAIL_ATTRIBUTE_NAME(elem)                                     \
+  (TANKER_DETAIL_PARAMETER_NAME(elem))
 
-#define TANKER_DETAIL_OPERATOR_EQUAL_LHS(unused1, size, idx, elem) \
-  lhs.TANKER_DETAIL_PARAMETER_NAME(elem)()                         \
-      BOOST_PP_COMMA_IF(BOOST_PP_LESS(BOOST_PP_ADD(idx, 1), size))
+#define TANKER_DETAIL_OPERATOR_EQUAL_LHS(unused1, unused2, idx, elem) \
+  BOOST_PP_COMMA_IF(idx)                                              \
+  lhs.TANKER_DETAIL_PARAMETER_NAME(elem)()
 
-#define TANKER_DETAIL_OPERATOR_EQUAL_RHS(unused1, size, idx, elem) \
-  rhs.TANKER_DETAIL_PARAMETER_NAME(elem)()                         \
-      BOOST_PP_COMMA_IF(BOOST_PP_LESS(BOOST_PP_ADD(idx, 1), size))
+#define TANKER_DETAIL_OPERATOR_EQUAL_RHS(unused1, unused2, idx, elem) \
+  BOOST_PP_COMMA_IF(idx)                                              \
+  rhs.TANKER_DETAIL_PARAMETER_NAME(elem)()
 
-#define TANKER_DETAIL_OPERATOR_EQUAL(list)                                  \
-  return std::tie(BOOST_PP_SEQ_FOR_EACH_I(TANKER_DETAIL_OPERATOR_EQUAL_LHS, \
-                                          BOOST_PP_SEQ_SIZE(list),          \
-                                          list)) ==                         \
-         std::tie(BOOST_PP_SEQ_FOR_EACH_I(TANKER_DETAIL_OPERATOR_EQUAL_RHS, \
-                                          BOOST_PP_SEQ_SIZE(list),          \
-                                          list));
+#define TANKER_DETAIL_OPERATOR_EQUAL(list)                                 \
+  return std::tie(BOOST_PP_SEQ_FOR_EACH_I(                                 \
+             TANKER_DETAIL_OPERATOR_EQUAL_LHS, BOOST_PP_EMPTY(), list)) == \
+         std::tie(BOOST_PP_SEQ_FOR_EACH_I(                                 \
+             TANKER_DETAIL_OPERATOR_EQUAL_RHS, BOOST_PP_EMPTY(), list));
 
-#define TANKER_DETAIL_DEFINE_CONSTRUCTOR(name, list)                          \
-  name(BOOST_PP_SEQ_FOR_EACH_I(                                               \
-      TANKER_DETAIL_CONSTRUCTOR_ARGS, BOOST_PP_SEQ_SIZE(list), list))         \
-    : BOOST_PP_SEQ_FOR_EACH_I(                                                \
-          TANKER_DETAIL_CONSTRUCTOR_INIT_LIST, BOOST_PP_SEQ_SIZE(list), list) \
-  {                                                                           \
+#define TANKER_DETAIL_DEFINE_CONSTRUCTOR(name, list)                   \
+  name(BOOST_PP_SEQ_FOR_EACH_I(                                        \
+      TANKER_DETAIL_CONSTRUCTOR_ARGS, BOOST_PP_EMPTY(), list))         \
+    : BOOST_PP_SEQ_FOR_EACH_I(                                         \
+          TANKER_DETAIL_CONSTRUCTOR_INIT_LIST, BOOST_PP_EMPTY(), list) \
+  {                                                                    \
   }
 
 #define TANKER_DETAIL_DEFINE_ACTION(name, list)                              \
