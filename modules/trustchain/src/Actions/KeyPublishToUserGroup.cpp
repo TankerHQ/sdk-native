f7a157af0d8069dfef00484116b2773eb94a545b
Théo DELRIEU <theo.delrieu@tanker.io>
2019-05-06 15:29:57 +0200

    trustchain: merge KeyPublishToUserGroup Json/Serialization extensions

--- modules/trustchain/src/Actions/KeyPublishToUserGroup.cpp
+++ modules/trustchain/src/Actions/KeyPublishToUserGroup.cpp
@@ -1,5 +1,9 @@
 #include <Tanker/Trustchain/Actions/KeyPublishToUserGroup.hpp>
 
+#include <Tanker/Serialization/Serialization.hpp>
+
+#include <nlohmann/json.hpp>
+
 namespace Tanker
 {
 namespace Trustchain
@@ -48,6 +52,28 @@ bool operator!=(KeyPublishToUserGroup const& lhs,
                 KeyPublishToUserGroup const& rhs)
 {
   return !(lhs == rhs);
+}
+
+void from_serialized(Serialization::SerializedSource& ss,
+                     KeyPublishToUserGroup& kp)
+{
+  Serialization::deserialize_to(ss, kp._recipientPublicEncryptionKey);
+  Serialization::deserialize_to(ss, kp._resourceId);
+  Serialization::deserialize_to(ss, kp._sealedSymmetricKey);
+}
+
+std::uint8_t* to_serialized(std::uint8_t* it, KeyPublishToUserGroup const& kp)
+{
+  it = Serialization::serialize(it, kp.recipientPublicEncryptionKey());
+  it = Serialization::serialize(it, kp.resourceId());
+  return Serialization::serialize(it, kp.sealedSymmetricKey());
+}
+
+void to_json(nlohmann::json& j, KeyPublishToUserGroup const& kp)
+{
+  j["recipientPublicEncryptionKey"] = kp.recipientPublicEncryptionKey();
+  j["resourceId"] = kp.resourceId();
+  j["key"] = kp.sealedSymmetricKey();
 }
 }
 }
