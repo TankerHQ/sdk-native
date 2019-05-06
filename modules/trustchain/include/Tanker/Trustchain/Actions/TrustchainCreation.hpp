559b284a820ffa2022c317d508bf0551b83f8a6b
Théo DELRIEU <theo.delrieu@tanker.io>
2019-05-06 15:38:56 +0200

    trustchain: merge TrustchainCreation Json/Serialization extensions

--- modules/trustchain/include/Tanker/Trustchain/Actions/TrustchainCreation.hpp
+++ modules/trustchain/include/Tanker/Trustchain/Actions/TrustchainCreation.hpp
@@ -4,6 +4,11 @@
 #include <Tanker/Serialization/SerializedSource.hpp>
 #include <Tanker/Trustchain/Actions/Nature.hpp>
 
+#include <nlohmann/json_fwd.hpp>
+
+#include <cstddef>
+#include <cstdint>
+
 namespace Tanker
 {
 namespace Trustchain
@@ -29,6 +34,16 @@ private:
 bool operator==(TrustchainCreation const& lhs, TrustchainCreation const& rhs);
 bool operator!=(TrustchainCreation const& lhs, TrustchainCreation const& rhs);
 
+void from_serialized(Serialization::SerializedSource&, TrustchainCreation&);
+std::uint8_t* to_serialized(std::uint8_t*, TrustchainCreation const&);
+
+constexpr std::size_t serialized_size(TrustchainCreation const&)
+{
+  return Crypto::PublicSignatureKey::arraySize;
+}
+
+void to_json(nlohmann::json&, TrustchainCreation const&);
+
 constexpr Nature TrustchainCreation::nature()
 {
   return Nature::TrustchainCreation;
@@ -36,6 +51,3 @@ constexpr Nature TrustchainCreation::nature()
 }
 }
 }
-
-#include <Tanker/Trustchain/Json/TrustchainCreation.hpp>
-#include <Tanker/Trustchain/Serialization/TrustchainCreation.hpp>
