b8d0e6d1742d02833d560038b12fec190268b737
Alexandre Bossard <alexandre.bossard@tanker.io>
2019-05-14 11:19:43 +0200

    compat: Split States from test file

---
+++ compat/dev/States.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <Tanker/Test/Functional/User.hpp>
+#include <Tanker/Types/SGroupId.hpp>
+
+#include <nlohmann/json_fwd.hpp>
+
+#include <optional.hpp>
+
+#include <string>
+#include <vector>
+
+struct EncryptState
+{
+  std::string clearData;
+  std::vector<uint8_t> encryptedData;
+};
+
+void to_json(nlohmann::json& j, EncryptState const& state);
+void from_json(nlohmann::json const& j, EncryptState& state);
+
+struct ShareState
+{
+  Tanker::Test::User alice;
+  Tanker::Test::User bob;
+  nonstd::optional<Tanker::SGroupId> groupId;
+  EncryptState encryptState;
+};
+
+void to_json(nlohmann::json& j, ShareState const& state);
+void from_json(nlohmann::json const& j, ShareState& state);
