ac7e6d6b8081dd8701d275b6e0497298c223b7f6
Alexandre Bossard <alexandre.bossard@tanker.io>
2019-05-15 16:42:52 +0200

    compat: Move helpers to their own files

--- compat/dev/Tests.hpp
+++ compat/dev/Tests.hpp
@@ -1,16 +1,12 @@
 #pragma once
 
 #include "Command.hpp"
+#include "Helpers.hpp"
 #include "States.hpp"
 
 #include <Tanker/AsyncCore.hpp>
 #include <Tanker/Identity/PublicIdentity.hpp>
-#include <Tanker/Trustchain/TrustchainId.hpp>
-#include <Tanker/Version.hpp>
 
-#include <Tanker/Test/Functional/TrustchainFactory.hpp>
-
-#include <Helpers/Buffers.hpp>
 #include <Helpers/JsonFile.hpp>
 
 #include <boost/filesystem/operations.hpp>
@@ -19,128 +15,7 @@
 using namespace std::string_literals;
 
 using Tanker::Compat::Command;
-using Tanker::Test::Trustchain;
-using Tanker::Test::TrustchainFactory;
 using Tanker::Test::User;
-using Tanker::Trustchain::TrustchainId;
-
-struct AsyncCoreDeleter
-{
-  void operator()(Tanker::AsyncCore* core) const
-  {
-    core->destroy().get();
-  }
-};
-
-using CorePtr = std::unique_ptr<Tanker::AsyncCore, AsyncCoreDeleter>;
-
-namespace
-{
-
-tc::future<Tanker::VerificationCode> getVerificationCode(
-    TrustchainId const& id, Tanker::Email const& email)
-{
-  return tc::async_resumable([=]() -> tc::cotask<Tanker::VerificationCode> {
-    auto tf = TC_AWAIT(TrustchainFactory::create());
-    TC_RETURN(TC_AWAIT(tf->getVerificationCode(id, email)));
-  });
-}
-
-CorePtr createCore(std::string const& url,
-                   Tanker::Trustchain::TrustchainId const& id,
-                   std::string const& tankerPath)
-{
-  return std::unique_ptr<Tanker::AsyncCore, AsyncCoreDeleter>(
-      new Tanker::AsyncCore(
-          url, Tanker::SdkInfo{"test", id, TANKER_VERSION}, tankerPath),
-      AsyncCoreDeleter{});
-}
-
-struct UserSession
-{
-  CorePtr core;
-  User user;
-};
-
-UserSession signUpUser(Tanker::Test::Trustchain& trustchain,
-                       std::string const& tankerPath)
-{
-  auto user = trustchain.makeUser();
-  auto core = createCore(trustchain.url, trustchain.id, tankerPath);
-  core->signUp(user.identity).get();
-  return {std::move(core), std::move(user)};
-}
-
-void claim(CorePtr& core,
-           Tanker::Test::Trustchain& trustchain,
-           Tanker::SSecretProvisionalIdentity const& provisionalIdentity,
-           std::string const& email)
-{
-  auto const verifCode =
-      getVerificationCode(trustchain.id, Tanker::Email{"bob@tanker.io"}).get();
-  core->claimProvisionalIdentity(provisionalIdentity, verifCode).get();
-}
-
-UserSession signUpProvisionalUser(
-    Tanker::SSecretProvisionalIdentity const& provisionalIdentity,
-    std::string const& email,
-    Tanker::Test::Trustchain& trustchain,
-    std::string const& tankerPath)
-{
-  auto session = signUpUser(trustchain, tankerPath);
-  claim(session.core, trustchain, provisionalIdentity, email);
-  return session;
-}
-
-CorePtr signInUser(std::string const& identity,
-                   Tanker::Test::Trustchain& trustchain,
-                   std::string const& tankerPath)
-{
-  auto core = createCore(trustchain.url, trustchain.id, tankerPath);
-  core->signIn(identity).get();
-  return core;
-}
-
-void decrypt(CorePtr const& core,
-             std::vector<uint8_t> const& encryptedData,
-             std::string const& expectedData)
-{
-  auto decryptedData = std::vector<uint8_t>(
-      Tanker::AsyncCore::decryptedSize(encryptedData).get());
-  core->decrypt(decryptedData.data(), encryptedData).get();
-  fmt::print(">> {}\n",
-             std::string(decryptedData.begin(), decryptedData.end()));
-  if (std::string(decryptedData.begin(), decryptedData.end()) != expectedData)
-    throw std::runtime_error("failed to decrypt");
-}
-
-std::vector<uint8_t> encrypt(CorePtr& core,
-                             std::string clearData,
-                             std::vector<Tanker::SPublicIdentity> users,
-                             std::vector<Tanker::SGroupId> groups)
-{
-  auto const buffer = Tanker::make_buffer(clearData);
-  auto encryptedData =
-      std::vector<uint8_t>(Tanker::AsyncCore::encryptedSize(clearData.size()));
-
-  core->encrypt(encryptedData.data(), buffer, users, groups).get();
-  return encryptedData;
-}
-
-User upgradeToIdentity(Tanker::Trustchain::TrustchainId const& trustchainId,
-                       User user)
-{
-  if (user.userToken)
-  {
-    user.identity = Tanker::Identity::upgradeUserToken(
-        cppcodec::base64_rfc4648::encode(trustchainId),
-        user.suserId,
-        user.userToken.value());
-    user.userToken.reset();
-  }
-  return user;
-}
-}
 
 struct EncryptCompat : Command
 {
