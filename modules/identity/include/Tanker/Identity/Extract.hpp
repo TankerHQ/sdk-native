9f6424240e31d30c9d7194583e5a28b3ee8aff05
Alexandre Bossard <alexandre.bossard@tanker.io>
2019-06-07 14:24:05 +0200

    Identity: Add missing exceptions

--- modules/identity/include/Tanker/Identity/Extract.hpp
+++ modules/identity/include/Tanker/Identity/Extract.hpp
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <Tanker/Errors/Exception.hpp>
+#include <Tanker/Identity/Errors/Errc.hpp>
+
+#include <cppcodec/base64_rfc4648.hpp>
 #include <nlohmann/json.hpp>
 
 namespace Tanker
@@ -14,7 +18,19 @@ nlohmann::json extract(std::string const& token);
 template <typename I>
 I extract(std::string const& token)
 {
-  return detail::extract(token).get<I>();
+  try
+  {
+    return detail::extract(token).get<I>();
+  }
+  catch (nlohmann::json::exception const&)
+  {
+    throw Errors::formatEx(Errc::InvalidFormat, "json deserialization failed");
+  }
+  catch (cppcodec::parse_error const&)
+  {
+    throw Errors::formatEx(Errc::InvalidFormat,
+                           "base64 deserialization failed");
+  }
 }
 }
 }
--- modules/identity/src/Extract.cpp
+++ modules/identity/src/Extract.cpp
@@ -1,10 +1,5 @@
 #include <Tanker/Identity/Extract.hpp>
 
-#include <Tanker/Errors/Exception.hpp>
-#include <Tanker/Identity/Errors/Errc.hpp>
-
-#include <cppcodec/base64_rfc4648.hpp>
-
 using namespace Tanker::Errors;
 
 namespace Tanker
@@ -15,18 +10,7 @@ namespace detail
 {
 nlohmann::json extract(std::string const& token)
 {
-  try
-  {
-    return nlohmann::json::parse(cppcodec::base64_rfc4648::decode(token));
-  }
-  catch (nlohmann::json::exception const&)
-  {
-    throw formatEx(Errc::InvalidFormat, "json deserialization failed");
-  }
-  catch (cppcodec::parse_error const&)
-  {
-    throw formatEx(Errc::InvalidFormat, "base64 deserialization failed");
-  }
+  return nlohmann::json::parse(cppcodec::base64_rfc4648::decode(token));
 }
 }
 }
