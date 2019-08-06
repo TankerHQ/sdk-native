ded174069995d9b1f0fcf9b19f08cd2de972dac4
Philippe Daouadi <philippe.daouadi@tanker.io>
2019-08-06 17:23:08 +0200

    FileKit: add upload/download methods

---
+++ modules/filekit/src/FileKit.cpp
@@ -0,0 +1,199 @@
+#include <Tanker/FileKit/FileKit.hpp>
+
+#include <Tanker/Crypto/Format/Format.hpp>
+#include <Tanker/Encryptor.hpp>
+
+#include <cppcodec/base64_rfc4648.hpp>
+
+#include <nlohmann/json.hpp>
+
+#include <optional.hpp>
+
+namespace Tanker
+{
+namespace FileKit
+{
+namespace
+{
+template <typename T>
+nonstd::optional<T> getJsonOptional(nlohmann::json const& j,
+                                    std::string const& name)
+{
+  auto it = j.find(name);
+  if (it == j.end())
+    return nonstd::nullopt;
+  if (it->is_null())
+    return nonstd::nullopt;
+  return it->get<T>();
+}
+}
+
+void from_json(nlohmann::json const& j, Metadata& m)
+{
+  m.mime = getJsonOptional<std::string>(j, "mime");
+  m.name = getJsonOptional<std::string>(j, "name");
+  auto const lastModified = getJsonOptional<uint64_t>(j, "lastModified");
+  if (lastModified)
+    m.lastModified = std::chrono::milliseconds(*lastModified);
+}
+void to_json(nlohmann::json& j, Metadata const& m)
+{
+  if (m.mime)
+    j["mime"] = *m.mime;
+  if (m.name)
+    j["name"] = *m.name;
+  if (m.lastModified)
+    j["lastModified"] = m.lastModified->count();
+}
+
+FileKit::FileKit(AsyncCore& core) : _core(core)
+{
+}
+
+tc::cotask<Trustchain::ResourceId> FileKit::upload(
+    gsl::span<uint8_t const> data,
+    Metadata const& metadata,
+    std::vector<SPublicIdentity> const& publicIdentities,
+    std::vector<SGroupId> const& groupIds)
+{
+  auto const encryptedMetadata =
+      TC_AWAIT(encryptMetadata(metadata, publicIdentities, groupIds));
+
+  auto const encryptedData =
+      TC_AWAIT(_core.encrypt(data, publicIdentities, groupIds));
+  auto const resourceId = Core::getResourceId(encryptedData);
+
+  auto const uploadTicket =
+      TC_AWAIT(_core.getFileUploadTicket(resourceId, encryptedData.size()));
+
+  if (uploadTicket.service != "GCS")
+    throw Errors::formatEx(Errors::Errc::InvalidArgument,
+                           "unsupported storage service: {}",
+                           uploadTicket.service);
+
+  auto const uploadUrl =
+      TC_AWAIT(getUploadUrl(uploadTicket, encryptedMetadata));
+  TC_AWAIT(performUploadRequest(uploadUrl, encryptedData));
+
+  TC_RETURN(resourceId);
+}
+
+tc::cotask<std::string> FileKit::encryptMetadata(
+    Metadata const& metadata,
+    std::vector<SPublicIdentity> const& publicIdentities,
+    std::vector<SGroupId> const& groupIds)
+{
+  auto const jmetadata = nlohmann::json(metadata).dump();
+  auto const encryptedMetadata =
+      TC_AWAIT(_core.encrypt(gsl::make_span(jmetadata).as_span<uint8_t const>(),
+                             publicIdentities,
+                             groupIds));
+  TC_RETURN(cppcodec::base64_rfc4648::encode(encryptedMetadata));
+}
+
+tc::cotask<std::string> FileKit::getUploadUrl(
+    CloudStorage::UploadTicket const& ticket,
+    std::string const& encryptedMetadata)
+{
+  auto const req = std::make_shared<tccurl::request>();
+  req->set_url(ticket.url);
+  for (auto const header : ticket.headers)
+    req->add_header(fmt::format("{}: {}", header.first, header.second));
+  req->add_header(
+      fmt::format("x-goog-meta-tanker-metadata: {}", encryptedMetadata));
+  curl_easy_setopt(req->get_curl(), CURLOPT_POST, 1L);
+  curl_easy_setopt(req->get_curl(), CURLOPT_POSTFIELDSIZE, 0L);
+
+  auto const result = TC_AWAIT(tccurl::read_all(multi, req));
+  if (!req->is_response_ok())
+    throw Errors::formatEx(Errors::Errc::NetworkError,
+                           "invalid status for initial upload request: {}",
+                           req->get_status_code());
+  TC_RETURN(result.header.at("location"));
+}
+
+tc::cotask<void> FileKit::performUploadRequest(std::string const& url,
+                                               gsl::span<uint8_t const> data)
+{
+  auto const req = std::make_shared<tccurl::request>();
+  req->set_url(url);
+
+  // Use CUSTOMREQUEST and not PUT or UPLOAD. This avoids an "Expect:
+  // 100-continue" roundtrip and makes use of POSTFIELDS instead of READDATA.
+  curl_easy_setopt(req->get_curl(), CURLOPT_CUSTOMREQUEST, "PUT");
+  curl_easy_setopt(req->get_curl(), CURLOPT_POSTFIELDS, data.data());
+  curl_easy_setopt(
+      req->get_curl(), CURLOPT_POSTFIELDSIZE, static_cast<long>(data.size()));
+  req->add_header("content-type:");
+  auto const dataUploadResult = TC_AWAIT(tccurl::read_all(multi, req));
+  if (!req->is_response_ok())
+    throw Errors::formatEx(Errors::Errc::NetworkError,
+                           "invalid status code for upload request: {}",
+                           req->get_status_code());
+}
+
+tc::cotask<std::pair<std::vector<uint8_t>, Metadata>> FileKit::download(
+    Trustchain::ResourceId const& resourceId)
+{
+  auto const downloadTicket = TC_AWAIT(_core.getFileDownloadTicket(resourceId));
+
+  if (downloadTicket.service != "GCS")
+    throw Errors::formatEx(Errors::Errc::InvalidArgument,
+                           "unsupported storage service: {}",
+                           downloadTicket.service);
+
+  auto const metadata = TC_AWAIT(decryptMetadata(
+      TC_AWAIT(downloadMetadata(resourceId, downloadTicket.url))));
+
+  auto const encryptedData =
+      TC_AWAIT(performDownloadRequest(downloadTicket.url));
+
+  TC_RETURN(std::make_pair(TC_AWAIT(_core.decrypt(encryptedData)), metadata));
+}
+
+tc::cotask<std::string> FileKit::downloadMetadata(
+    Trustchain::ResourceId const& resourceId, std::string const& url)
+{
+  auto const req = std::make_shared<tccurl::request>();
+  req->set_url(url);
+  curl_easy_setopt(req->get_curl(), CURLOPT_NOBODY, 1L);
+
+  auto const result = TC_AWAIT(tccurl::read_all(multi, req));
+  if (req->get_status_code() == 404)
+    throw Errors::formatEx(
+        Errors::Errc::InvalidArgument,
+        "could not find a file with the following resource ID: {}",
+        resourceId);
+  if (!req->is_response_ok())
+    throw Errors::formatEx(Errors::Errc::NetworkError,
+                           "invalid status for download HEAD request: {}",
+                           req->get_status_code());
+  TC_RETURN(result.header.at("x-goog-meta-tanker-metadata"));
+}
+
+tc::cotask<Metadata> FileKit::decryptMetadata(
+    std::string const& sencryptedMetadata)
+{
+  auto const encryptedMetadata =
+      cppcodec::base64_rfc4648::decode(sencryptedMetadata);
+  auto const decryptedMetadata = TC_AWAIT(_core.decrypt(encryptedMetadata));
+  TC_RETURN(
+      nlohmann::json::parse(decryptedMetadata.begin(), decryptedMetadata.end())
+          .get<Metadata>());
+}
+
+tc::cotask<std::vector<uint8_t>> FileKit::performDownloadRequest(
+    std::string const& url)
+{
+  auto const req = std::make_shared<tccurl::request>();
+  req->set_url(url);
+
+  auto const result = TC_AWAIT(tccurl::read_all(multi, req));
+  if (!req->is_response_ok())
+    throw Errors::formatEx(Errors::Errc::NetworkError,
+                           "invalid status for download GET request: {}",
+                           req->get_status_code());
+  TC_RETURN(result.data);
+}
+}
+}
