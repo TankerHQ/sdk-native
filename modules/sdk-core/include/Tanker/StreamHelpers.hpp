59d12a4bfca51af222d8b9ba3d18d9832e2173c1
Philippe Daouadi <philippe.daouadi@tanker.io>
2019-09-09 14:09:16 +0200

    StreamHelpers: add bufferToInputSource

--- modules/sdk-core/include/Tanker/StreamHelpers.hpp
+++ modules/sdk-core/include/Tanker/StreamHelpers.hpp
@@ -8,17 +8,31 @@
 
 namespace Tanker
 {
-inline auto bufferViewToInputSource(gsl::span<std::uint8_t const> buffer)
+namespace detail
 {
-  return
-      [index = 0u, buffer](std::uint8_t* out,
-                           std::int64_t n) mutable -> tc::cotask<std::int64_t> {
-        auto const toRead =
-            std::min(n, static_cast<std::int64_t>(buffer.size()) - index);
-        std::copy_n(buffer.data() + index, toRead, out);
-        index += toRead;
-        TC_RETURN(toRead);
-      };
+template <typename T>
+inline auto bufferToInputSource(T&& buffer)
+{
+  return [index = 0u, buffer = std::forward<T>(buffer)](
+             std::uint8_t* out,
+             std::int64_t n) mutable -> tc::cotask<std::int64_t> {
+    auto const toRead =
+        std::min(n, static_cast<std::int64_t>(buffer.size()) - index);
+    std::copy_n(buffer.data() + index, toRead, out);
+    index += toRead;
+    TC_RETURN(toRead);
+  };
+}
+}
+
+inline auto bufferViewToInputSource(gsl::span<uint8_t const> buffer)
+{
+  return detail::bufferToInputSource(buffer);
+}
+
+inline auto bufferToInputSource(std::vector<uint8_t> buffer)
+{
+  return detail::bufferToInputSource(std::move(buffer));
 }
 
 inline tc::cotask<int64_t> readStream(gsl::span<uint8_t> out,
