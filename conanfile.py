eb5a50ff1291da5113f153afe4552d1d80433a3e
Théo DELRIEU <theo.delrieu@tanker.io>
2019-09-10 11:56:44 +0200

    conanfile: export cmake files

--- conanfile.py
+++ conanfile.py
@@ -16,7 +16,7 @@ class TankerConan(ConanFile):
         "coroutinests": [True, False],
         }
     default_options = "tankerlib_shared=False", "fPIC=True", "with_ssl=True", "with_tracer=False", "warn_as_error=False", "sanitizer=None", "coverage=False", "coroutinests=False"
-    exports_sources = "CMakeLists.txt", "modules/*"
+    exports_sources = "CMakeLists.txt", "modules/*", "cmake/*"
     generators = "cmake", "json", "ycm"
     cmake = None
 
