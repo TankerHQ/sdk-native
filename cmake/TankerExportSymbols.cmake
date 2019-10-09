6ea205f5b3fe0feba040af3ebfbbf7e140e1d4be
Théo DELRIEU <theo.delrieu@tanker.io>
2019-10-09 11:05:04 +0200

    cmake: use linux linker script for mingw builds

--- cmake/TankerExportSymbols.cmake
+++ cmake/TankerExportSymbols.cmake
@@ -2,10 +2,10 @@ macro(tanker_export_symbols target)
   if (APPLE)
     set_target_properties(${target} PROPERTIES
       LINK_FLAGS "-Wl,-exported_symbols_list ${CMAKE_CURRENT_LIST_DIR}/exported_symbols.sym")
-  elseif (UNIX)
+  elseif (UNIX OR MINGW)
     set_target_properties(${target} PROPERTIES
       LINK_FLAGS "-Wl,--version-script=${CMAKE_CURRENT_LIST_DIR}/exported_symbols.ld")
-  elseif (WIN32)
+  elseif (MSVC)
     set_target_properties(${target} PROPERTIES
       LINK_FLAGS "/DEF:${CMAKE_CURRENT_LIST_DIR}/exported_symbols.def")
   else ()
