2444588b2477a2d880b60b77aaaa6d0fc50e95cc
Théo DELRIEU <theo.delrieu@tanker.io>
2020-01-15 14:39:33 +0100

    trustchain: add Context class

---
+++ modules/trustchain/src/Context.cpp
@@ -0,0 +1,20 @@
+#include <Tanker/Trustchain/Context.hpp>
+
+namespace Tanker::Trustchain
+{
+Context::Context(TrustchainId const& id,
+                 Crypto::PublicSignatureKey const& publicSignatureKey)
+  : _id(id), _publicSignatureKey(publicSignatureKey)
+{
+}
+
+TrustchainId const& Context::id() const
+{
+  return _id;
+}
+
+Crypto::PublicSignatureKey const& Context::publicSignatureKey() const
+{
+  return _publicSignatureKey;
+}
+}
