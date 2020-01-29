dc3ea910d249a499e71f72dace31df713b478d3d
Dimitri Merejkowsky <dimitri.merejkowsky@tanker.io>
2020-01-29 16:52:43 +0100

    style(end2end): format with black

--- end2end.py
+++ end2end.py
@@ -13,16 +13,31 @@ def export_tanker_dev(src_path: Path) -> None:
     ci.conan.export(src_path=src_path, ref_or_channel="tanker/dev")
 
 
-def use_packaged_tanker(src_path: Path,  profile: str) -> None:
-    builder = ci.cpp.Builder(src_path, profile=profile,
-                             make_package=False, coverage=False, warn_as_error=False)
+def use_packaged_tanker(src_path: Path, profile: str) -> None:
+    builder = ci.cpp.Builder(
+        src_path,
+        profile=profile,
+        make_package=False,
+        coverage=False,
+        warn_as_error=False,
+    )
     builder.export_pkg("tanker/dev")
 
 
 def main() -> None:
     parser = argparse.ArgumentParser()
-    parser.add_argument("--isolate-conan-user-home", action="store_true", dest="home_isolation", default=False)
-    parser.add_argument("--export-tanker-dev", action="store_true", dest="export_tanker_dev", default=False)
+    parser.add_argument(
+        "--isolate-conan-user-home",
+        action="store_true",
+        dest="home_isolation",
+        default=False,
+    )
+    parser.add_argument(
+        "--export-tanker-dev",
+        action="store_true",
+        dest="export_tanker_dev",
+        default=False,
+    )
     parser.add_argument("--profile", required=True)
     parser.add_argument("--use-local-sources", action="store_true", default=False)
 
@@ -47,8 +62,9 @@ def main() -> None:
         tanker_conan_ref="tanker/dev@tanker/dev",
         profile=args.profile,
         base_path=base_path,
-        project_config="dev"
+        project_config="dev",
     )
+
 
 if __name__ == "__main__":
     main()
