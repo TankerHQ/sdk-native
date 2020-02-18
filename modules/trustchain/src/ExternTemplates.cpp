a723bd38342aaf3014c73150d12d00e08194c44a
Alexandre Bossard <alexandre.bossard@tanker.io>
2020-02-18 10:49:44 +0100

    feat: Add HashedPassphrase type

--- modules/trustchain/src/ExternTemplates.cpp
+++ modules/trustchain/src/ExternTemplates.cpp
@@ -1,5 +1,6 @@
 #include <Tanker/Trustchain/DeviceId.hpp>
 #include <Tanker/Trustchain/GroupId.hpp>
+#include <Tanker/Trustchain/HashedPassphrase.hpp>
 #include <Tanker/Trustchain/ResourceId.hpp>
 #include <Tanker/Trustchain/TrustchainId.hpp>
 #include <Tanker/Trustchain/UserId.hpp>
@@ -11,6 +12,7 @@ namespace Crypto
 template class BasicHash<struct Trustchain::detail::DeviceIdImpl>;
 template class BasicHash<struct Trustchain::detail::UserIdImpl>;
 template class BasicHash<struct Trustchain::detail::TrustchainIdImpl>;
+template class BasicHash<struct Trustchain::detail::HashedPassphraseImpl>;
 template class BasicCryptographicType<Trustchain::GroupId,
                                       PublicSignatureKey::arraySize>;
 template class BasicCryptographicType<Trustchain::ResourceId, Mac::arraySize>;
