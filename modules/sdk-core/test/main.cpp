8cdc78afa46c26eea63fe427b86550ccaddbe152
Alexandre Bossard <alexandre.bossard@tanker.io>
2020-02-24 18:04:25 +0100

    chore: bump trompeloeil to v37

--- modules/sdk-core/test/main.cpp
+++ modules/sdk-core/test/main.cpp
@@ -14,20 +14,11 @@ int main(int argc, char* argv[])
                                const char* file,
                                unsigned long line,
                                std::string const& msg) {
-    std::ostringstream os;
-    if (line)
-      os << file << ':' << line << '\n';
-    os << msg;
-    auto failure = os.str();
+    auto f = line ? file : "[file/line unavailable]";
     if (s == trompeloeil::severity::fatal)
-    {
-      FAIL(failure);
-    }
+      ADD_FAIL_AT(f, line, msg);
     else
-    {
-      CAPTURE(failure);
-      CHECK(failure.empty());
-    }
+      ADD_FAIL_CHECK_AT(f, line, msg);
   });
 
   doctest::Context context(argc, argv);
