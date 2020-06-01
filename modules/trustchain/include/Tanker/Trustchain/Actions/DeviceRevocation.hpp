92d7be2d75b9c4298d7924deaeb04d9325cd9a99
Philippe Daouadi <philippe.daouadi@tanker.io>
2020-06-01 16:01:45 +0200

    DeviceRevocation: use macros

--- modules/trustchain/include/Tanker/Trustchain/Actions/DeviceRevocation.hpp
+++ modules/trustchain/include/Tanker/Trustchain/Actions/DeviceRevocation.hpp
@@ -7,6 +7,7 @@
 #include <Tanker/Trustchain/Actions/DeviceRevocation/v2.hpp>
 #include <Tanker/Trustchain/Actions/Nature.hpp>
 #include <Tanker/Trustchain/DeviceId.hpp>
+#include <Tanker/Trustchain/Preprocessor/Actions/VariantImplementation.hpp>
 
 #include <boost/variant2/variant.hpp>
 #include <nlohmann/json_fwd.hpp>
@@ -24,40 +25,22 @@ namespace Actions
 {
 class DeviceRevocation
 {
+  TANKER_TRUSTCHAIN_ACTION_VARIANT_IMPLEMENTATION(DeviceRevocation,
+                                                  (DeviceRevocation1,
+                                                   DeviceRevocation2),
+                                                  (deviceId, DeviceId))
+
 public:
   using v1 = DeviceRevocation1;
   using v2 = DeviceRevocation2;
 
-  DeviceRevocation() = default;
-  DeviceRevocation(v1 const&);
-  DeviceRevocation(v2 const&);
-
   Nature nature() const;
-  DeviceId const& deviceId() const;
-
-  template <typename T>
-  bool holds_alternative() const;
-
-  template <typename T>
-  T const& get() const;
-
-  template <typename T>
-  T const* get_if() const;
-
-  template <typename Callable>
-  decltype(auto) visit(Callable&&) const;
 
 private:
-  boost::variant2::variant<v1, v2> _variant;
-
-  friend bool operator==(DeviceRevocation const&, DeviceRevocation const&);
   friend std::uint8_t* to_serialized(std::uint8_t*, DeviceRevocation const&);
   friend std::size_t serialized_size(DeviceRevocation const&);
   friend void to_json(nlohmann::json&, DeviceRevocation const&);
 };
-
-bool operator==(DeviceRevocation const&, DeviceRevocation const&);
-bool operator!=(DeviceRevocation const&, DeviceRevocation const&);
 
 // The nature is not present in the wired payload.
 // Therefore there is no from_serialized overload for DeviceRevocation.
@@ -65,30 +48,6 @@ std::uint8_t* to_serialized(std::uint8_t*, DeviceRevocation const&);
 std::size_t serialized_size(DeviceRevocation const&);
 
 void to_json(nlohmann::json&, DeviceRevocation const&);
-
-template <typename T>
-bool DeviceRevocation::holds_alternative() const
-{
-  return boost::variant2::holds_alternative<T>(_variant);
-}
-
-template <typename T>
-T const& DeviceRevocation::get() const
-{
-  return boost::variant2::get<T>(_variant);
-}
-
-template <typename T>
-T const* DeviceRevocation::get_if() const
-{
-  return boost::variant2::get_if<T>(&_variant);
-}
-
-template <typename Callable>
-decltype(auto) DeviceRevocation::visit(Callable&& c) const
-{
-  return boost::variant2::visit(std::forward<Callable>(c), _variant);
-}
 }
 }
 }
