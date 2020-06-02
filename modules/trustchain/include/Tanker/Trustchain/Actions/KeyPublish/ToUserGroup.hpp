d5793abf609d6fd11216705c1e178ad4d6ecbc14
Philippe Daouadi <philippe.daouadi@tanker.io>
2020-06-02 14:10:05 +0200

    KeyPublishToUserGroup: use macros

--- modules/trustchain/include/Tanker/Trustchain/Actions/KeyPublish/ToUserGroup.hpp
+++ modules/trustchain/include/Tanker/Trustchain/Actions/KeyPublish/ToUserGroup.hpp
@@ -4,6 +4,9 @@
 #include <Tanker/Crypto/SealedSymmetricKey.hpp>
 #include <Tanker/Serialization/SerializedSource.hpp>
 #include <Tanker/Trustchain/Actions/Nature.hpp>
+#include <Tanker/Trustchain/Preprocessor/Actions/Implementation.hpp>
+#include <Tanker/Trustchain/Preprocessor/Actions/Json.hpp>
+#include <Tanker/Trustchain/Preprocessor/Actions/Serialization.hpp>
 #include <Tanker/Trustchain/ResourceId.hpp>
 
 #include <nlohmann/json_fwd.hpp>
@@ -17,47 +20,33 @@ namespace Trustchain
 {
 namespace Actions
 {
+#define TANKER_TRUSTCHAIN_ACTIONS_KEY_PUBLISH_TO_USER_GROUP_ATTRIBUTES \
+  (recipientPublicEncryptionKey, Crypto::PublicEncryptionKey),         \
+      (resourceId, ResourceId),                                        \
+      (sealedSymmetricKey, Crypto::SealedSymmetricKey)
+
 class KeyPublishToUserGroup
 {
 public:
-  KeyPublishToUserGroup() = default;
-  KeyPublishToUserGroup(Crypto::PublicEncryptionKey const&,
-                        ResourceId const&,
-                        Crypto::SealedSymmetricKey const&);
+  TANKER_IMMUTABLE_DATA_TYPE_IMPLEMENTATION(
+      KeyPublishToUserGroup,
+      TANKER_TRUSTCHAIN_ACTIONS_KEY_PUBLISH_TO_USER_GROUP_ATTRIBUTES)
 
+public:
   static constexpr Nature nature();
 
-  Crypto::PublicEncryptionKey const& recipientPublicEncryptionKey() const;
-  ResourceId const& resourceId() const;
-  Crypto::SealedSymmetricKey const& sealedSymmetricKey() const;
-
 private:
-  Crypto::PublicEncryptionKey _recipientPublicEncryptionKey;
-  ResourceId _resourceId;
-  Crypto::SealedSymmetricKey _sealedSymmetricKey;
-
   friend void from_serialized(Serialization::SerializedSource&,
                               KeyPublishToUserGroup&);
 };
-
-bool operator==(KeyPublishToUserGroup const&, KeyPublishToUserGroup const&);
-bool operator!=(KeyPublishToUserGroup const&, KeyPublishToUserGroup const&);
-
-void from_serialized(Serialization::SerializedSource&, KeyPublishToUserGroup&);
-std::uint8_t* to_serialized(std::uint8_t*, KeyPublishToUserGroup const&);
-
-constexpr std::size_t serialized_size(KeyPublishToUserGroup const&)
-{
-  return Crypto::PublicEncryptionKey::arraySize + ResourceId::arraySize +
-         Crypto::SealedSymmetricKey::arraySize;
-}
-
-void to_json(nlohmann::json&, KeyPublishToUserGroup const&);
 
 constexpr Nature KeyPublishToUserGroup::nature()
 {
   return Nature::KeyPublishToUserGroup;
 }
+
+TANKER_TRUSTCHAIN_ACTION_DECLARE_SERIALIZATION(KeyPublishToUserGroup)
+TANKER_TRUSTCHAIN_ACTION_DECLARE_TO_JSON(KeyPublishToUserGroup)
 }
 }
 }
--- modules/trustchain/src/Actions/KeyPublish/ToUserGroup.cpp
+++ modules/trustchain/src/Actions/KeyPublish/ToUserGroup.cpp
@@ -10,71 +10,12 @@ namespace Trustchain
 {
 namespace Actions
 {
-KeyPublishToUserGroup::KeyPublishToUserGroup(
-    Crypto::PublicEncryptionKey const& recipientPublicEncryptionKey,
-    ResourceId const& resourceId,
-    Crypto::SealedSymmetricKey const& sealedSymmetricKey)
-  : _recipientPublicEncryptionKey(recipientPublicEncryptionKey),
-    _resourceId(resourceId),
-    _sealedSymmetricKey(sealedSymmetricKey)
-{
-}
-
-Crypto::PublicEncryptionKey const&
-KeyPublishToUserGroup::recipientPublicEncryptionKey() const
-{
-  return _recipientPublicEncryptionKey;
-}
-
-ResourceId const& KeyPublishToUserGroup::resourceId() const
-{
-  return _resourceId;
-}
-
-Crypto::SealedSymmetricKey const& KeyPublishToUserGroup::sealedSymmetricKey()
-    const
-{
-  return _sealedSymmetricKey;
-}
-
-bool operator==(KeyPublishToUserGroup const& lhs,
-                KeyPublishToUserGroup const& rhs)
-{
-  return std::tie(lhs.recipientPublicEncryptionKey(),
-                  lhs.resourceId(),
-                  lhs.sealedSymmetricKey()) ==
-         std::tie(rhs.recipientPublicEncryptionKey(),
-                  rhs.resourceId(),
-                  rhs.sealedSymmetricKey());
-}
-
-bool operator!=(KeyPublishToUserGroup const& lhs,
-                KeyPublishToUserGroup const& rhs)
-{
-  return !(lhs == rhs);
-}
-
-void from_serialized(Serialization::SerializedSource& ss,
-                     KeyPublishToUserGroup& kp)
-{
-  Serialization::deserialize_to(ss, kp._recipientPublicEncryptionKey);
-  Serialization::deserialize_to(ss, kp._resourceId);
-  Serialization::deserialize_to(ss, kp._sealedSymmetricKey);
-}
-
-std::uint8_t* to_serialized(std::uint8_t* it, KeyPublishToUserGroup const& kp)
-{
-  it = Serialization::serialize(it, kp.recipientPublicEncryptionKey());
-  it = Serialization::serialize(it, kp.resourceId());
-  return Serialization::serialize(it, kp.sealedSymmetricKey());
-}
-
-void to_json(nlohmann::json& j, KeyPublishToUserGroup const& kp)
-{
-  j["recipientPublicEncryptionKey"] = kp.recipientPublicEncryptionKey();
-  j["resourceId"] = kp.resourceId();
-  j["key"] = kp.sealedSymmetricKey();
-}
+TANKER_TRUSTCHAIN_ACTION_DEFINE_SERIALIZATION(
+    KeyPublishToUserGroup,
+    TANKER_TRUSTCHAIN_ACTIONS_KEY_PUBLISH_TO_USER_GROUP_ATTRIBUTES)
+TANKER_TRUSTCHAIN_ACTION_DEFINE_TO_JSON(
+    KeyPublishToUserGroup,
+    TANKER_TRUSTCHAIN_ACTIONS_KEY_PUBLISH_TO_USER_GROUP_ATTRIBUTES)
 }
 }
 }
