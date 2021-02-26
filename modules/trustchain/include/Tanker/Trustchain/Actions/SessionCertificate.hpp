686808e43e3d3867a89756da99ddae8850e5e472
Timothée Isnard <timothee.isnard@tanker.io>
2021-02-26 15:38:51 +0100

    feat(2fa): Add SessionCertificate block and tests

---
+++ modules/trustchain/include/Tanker/Trustchain/Actions/SessionCertificate.hpp
@@ -0,0 +1,72 @@
+#pragma once
+
+#include <Tanker/Crypto/BasicCryptographicType.hpp>
+#include <Tanker/Crypto/PrivateEncryptionKey.hpp>
+#include <Tanker/Crypto/PrivateSignatureKey.hpp>
+#include <Tanker/Crypto/PublicEncryptionKey.hpp>
+#include <Tanker/Crypto/SealedPrivateEncryptionKey.hpp>
+#include <Tanker/Crypto/SealedPrivateSignatureKey.hpp>
+#include <Tanker/Crypto/SignatureKeyPair.hpp>
+#include <Tanker/Crypto/TwoTimesSealedPrivateEncryptionKey.hpp>
+#include <Tanker/Trustchain/DeviceId.hpp>
+#include <Tanker/Trustchain/Preprocessor/Actions/Implementation.hpp>
+#include <Tanker/Trustchain/UserId.hpp>
+
+#include <tuple>
+
+namespace Tanker
+{
+namespace Trustchain
+{
+namespace Actions
+{
+enum class VerificationMethodType : uint32_t
+{
+  Email = 1,
+  Passphrase = 2,
+  VerificationKey = 3,
+  OidcIdToken = 4,
+};
+
+std::uint8_t* to_serialized(std::uint8_t* it, VerificationMethodType const& mt);
+void from_serialized(Serialization::SerializedSource& ss,
+                     VerificationMethodType& mt);
+constexpr std::size_t serialized_size(VerificationMethodType const& mt)
+{
+  return Serialization::varint_size(static_cast<uint32_t>(mt));
+}
+
+#define TANKER_TRUSTCHAIN_ACTIONS_SESSION_CERTIFICATE_ATTRIBUTES               \
+  (sessionPublicSignatureKey, Crypto::PublicSignatureKey),                     \
+      (timestamp, uint64_t), (verificationMethodType, VerificationMethodType), \
+      (verificationMethodTarget, Crypto::Hash)
+
+class SessionCertificate
+{
+public:
+  TANKER_IMMUTABLE_ACTION_IMPLEMENTATION(
+      SessionCertificate,
+      TANKER_TRUSTCHAIN_ACTIONS_SESSION_CERTIFICATE_ATTRIBUTES)
+
+public:
+  SessionCertificate(
+      TrustchainId const& trustchainId,
+      Crypto::PublicSignatureKey const& sessionPublicSignatureKey,
+      uint64_t timestamp,
+      VerificationMethodType verificationMethodType,
+      Crypto::Hash const& verificationMethodTarget,
+      Crypto::Hash const& author,
+      Crypto::PrivateSignatureKey const& devicePrivateSignatureKey);
+
+  std::vector<std::uint8_t> signatureData() const;
+
+private:
+  friend void from_serialized(Serialization::SerializedSource&,
+                              SessionCertificate&);
+};
+
+TANKER_TRUSTCHAIN_ACTION_DECLARE_SERIALIZATION(SessionCertificate)
+TANKER_TRUSTCHAIN_ACTION_DECLARE_TO_JSON(SessionCertificate)
+}
+}
+}
---
+++ modules/trustchain/src/Actions/SessionCertificate.cpp
@@ -0,0 +1,69 @@
+#include <Tanker/Trustchain/Actions/SessionCertificate.hpp>
+
+#include <Tanker/Crypto/Crypto.hpp>
+#include <Tanker/Serialization/Serialization.hpp>
+#include <Tanker/Trustchain/Serialization.hpp>
+
+#include <nlohmann/json.hpp>
+
+#include <algorithm>
+
+namespace Tanker
+{
+namespace Trustchain
+{
+namespace Actions
+{
+SessionCertificate::SessionCertificate(
+    TrustchainId const& trustchainId,
+    Crypto::PublicSignatureKey const& sessionPublicSignatureKey,
+    uint64_t timestamp,
+    VerificationMethodType verificationMethodType,
+    Crypto::Hash const& verificationMethodTarget,
+    Crypto::Hash const& author,
+    Crypto::PrivateSignatureKey const& devicePrivateSignatureKey)
+  : _trustchainId(trustchainId),
+    _sessionPublicSignatureKey(sessionPublicSignatureKey),
+    _timestamp(timestamp),
+    _verificationMethodType(verificationMethodType),
+    _verificationMethodTarget(verificationMethodTarget),
+    _author(author),
+    _hash(computeHash()),
+    _signature(Crypto::sign(_hash, devicePrivateSignatureKey))
+{
+}
+
+std::vector<std::uint8_t> SessionCertificate::signatureData() const
+{
+  std::vector<std::uint8_t> signatureData(
+      8 + Crypto::PublicSignatureKey::arraySize +
+      Serialization::varint_size((uint32_t)_verificationMethodType) +
+      Crypto::Hash::arraySize);
+
+  auto it = std::copy(_sessionPublicSignatureKey.begin(),
+                      _sessionPublicSignatureKey.end(),
+                      &*signatureData.begin());
+  it = std::copy((char*)_timestamp, (char*)_timestamp + 8, it);
+  it = Serialization::varint_write(&*it, (uint32_t)_verificationMethodType);
+  std::copy(
+      _verificationMethodTarget.begin(), _verificationMethodTarget.end(), it);
+  return signatureData;
+}
+
+std::uint8_t* to_serialized(std::uint8_t* it, VerificationMethodType const& mt)
+{
+  return Serialization::varint_write(it, static_cast<uint32_t>(mt));
+}
+
+void from_serialized(Serialization::SerializedSource& ss,
+                     VerificationMethodType& mt)
+{
+  mt = static_cast<VerificationMethodType>(ss.read_varint());
+}
+
+TANKER_TRUSTCHAIN_ACTION_DEFINE_METHODS(
+    SessionCertificate,
+    TANKER_TRUSTCHAIN_ACTIONS_SESSION_CERTIFICATE_ATTRIBUTES)
+}
+}
+}
