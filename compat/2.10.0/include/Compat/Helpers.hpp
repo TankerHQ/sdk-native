e500f0b61a52e3274a81745ed1fe286bec920c1a
Philippe Daouadi <philippe.daouadi@tanker.io>
2021-03-03 10:01:33 +0100

    compat: add compat tests for v2.10.0

---
+++ compat/2.10.0/include/Compat/Helpers.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <Tanker/AsyncCore.hpp>
+
+#include <Tanker/Functional/Trustchain.hpp>
+#include <Tanker/Functional/User.hpp>
+
+struct AsyncCoreDeleter
+{
+  void operator()(Tanker::AsyncCore* core) const
+  {
+    core->destroy().get();
+  }
+};
+
+using CorePtr = std::unique_ptr<Tanker::AsyncCore, AsyncCoreDeleter>;
+
+CorePtr createCore(std::string const& url,
+                   Tanker::Trustchain::TrustchainId const& id,
+                   std::string const& tankerPath);
+
+struct UserSession
+{
+  CorePtr core;
+  Tanker::Functional::User user;
+};
+
+UserSession signUpUser(Tanker::Functional::Trustchain& trustchain,
+                       std::string const& tankerPath);
+
+void claim(CorePtr& core,
+           Tanker::Functional::Trustchain& trustchain,
+           Tanker::SSecretProvisionalIdentity const& provisionalIdentity,
+           std::string const& email,
+           std::string const& verifCode);
+
+UserSession signUpAndClaim(
+    Tanker::SSecretProvisionalIdentity const& provisionalIdentity,
+    std::string const& email,
+    std::string const& verifCode,
+    Tanker::Functional::Trustchain& trustchain,
+    std::string const& tankerPath);
+
+CorePtr signInUser(std::string const& identity,
+                   Tanker::Functional::Trustchain& trustchain,
+                   std::string const& tankerPath);
+
+void decryptAndCheck(CorePtr const& core,
+                     std::vector<uint8_t> const& encryptedData,
+                     std::string const& expectedData);
---
+++ compat/2.10.0/tests/EncryptSession.cpp
@@ -0,0 +1,51 @@
+#include <Compat/Command.hpp>
+#include <Compat/Helpers.hpp>
+#include <Compat/States.hpp>
+
+#include <Tanker/AsyncCore.hpp>
+#include <Tanker/EncryptionSession.hpp>
+#include <Tanker/Identity/PublicIdentity.hpp>
+
+#include <Helpers/Buffers.hpp>
+#include <Helpers/JsonFile.hpp>
+
+#include <nlohmann/json.hpp>
+
+using Tanker::Compat::Command;
+struct EncryptSession : Command
+{
+  using Command::Command;
+
+  void base() override
+  {
+    auto alice = signUpUser(trustchain, tankerPath);
+
+    auto clearData = std::string("my confession to bob");
+    auto encryptionSession = alice.core->makeEncryptionSession({}, {}).get();
+    std::vector<uint8_t> encryptedData(
+        Tanker::EncryptionSession::encryptedSize(clearData.size()));
+    tc::async_resumable([&]() -> tc::cotask<void> {
+      TC_AWAIT(encryptionSession.encrypt(encryptedData.data(),
+                                         Tanker::make_buffer(clearData)));
+    }).get();
+
+    Tanker::saveJson(statePath,
+                     ShareState{alice.user,
+                                {},
+                                std::nullopt,
+                                EncryptState{clearData, encryptedData}});
+  }
+
+  void next() override
+  {
+    auto const state = Tanker::loadJson(statePath).get<ShareState>();
+
+    auto aliceCore = signInUser(state.alice.identity, trustchain, tankerPath);
+    decryptAndCheck(aliceCore,
+                    state.encryptState.encryptedData,
+                    state.encryptState.clearData);
+  }
+};
+REGISTER_CMD(EncryptSession,
+             "encryption-session",
+             "encrypt in a session then decrypt");
