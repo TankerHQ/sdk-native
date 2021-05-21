3e5ba985d15e83b46d8842122bb8b0eaca801e31
Théo DELRIEU <theo.delrieu@tanker.io>
2021-05-21 10:01:09 +0200

    compat: add 2.12.0 tests

--- compat/run-compat.py
+++ compat/run-compat.py
@@ -24,6 +24,17 @@ TESTS = {
         "provisional-user-group-old-claim",
         "claim-provisional-self",
     ],
+    "2.12.0": [
+        "encrypt",
+        "encryption-session",
+        "group",
+        "unlock",
+        "preshare-and-claim",
+        "decrypt-old-claim",
+        "provisional-user-group-claim",
+        "provisional-user-group-old-claim",
+        "claim-provisional-self",
+    ],
     "2.11.0": [
         "encrypt",
         "encryption-session",
