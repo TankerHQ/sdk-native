1e00927386c6a91e9649fd5fc191580b47f01887
Théo Delrieu <theo.delrieu@tanker.io>
2021-09-20 11:26:25 +0200

    trustchain: make ProvisionalUserId totally_ordered

--- modules/trustchain/src/ProvisionalUserId.cpp
+++ modules/trustchain/src/ProvisionalUserId.cpp
@@ -12,6 +12,24 @@ bool operator<(ProvisionalUserId const& l, ProvisionalUserId const& r)
          std::tie(r.appSignaturePublicKey(), r.tankerSignaturePublicKey());
 }
 
+bool operator>(ProvisionalUserId const& l, ProvisionalUserId const& r)
+{
+  return std::tie(l.appSignaturePublicKey(), l.tankerSignaturePublicKey()) >
+         std::tie(r.appSignaturePublicKey(), r.tankerSignaturePublicKey());
+}
+
+bool operator>=(ProvisionalUserId const& l, ProvisionalUserId const& r)
+{
+  return std::tie(l.appSignaturePublicKey(), l.tankerSignaturePublicKey()) >=
+         std::tie(r.appSignaturePublicKey(), r.tankerSignaturePublicKey());
+}
+
+bool operator<=(ProvisionalUserId const& l, ProvisionalUserId const& r)
+{
+  return std::tie(l.appSignaturePublicKey(), l.tankerSignaturePublicKey()) <=
+         std::tie(r.appSignaturePublicKey(), r.tankerSignaturePublicKey());
+}
+
 TANKER_TRUSTCHAIN_DATA_DEFINE_SERIALIZATION(
     ProvisionalUserId, TANKER_TRUSTCHAIN_ACTIONS_PROVISIONAL_USER_ID_ATTRIBUTES)
 TANKER_TRUSTCHAIN_DATA_DEFINE_TO_JSON(
