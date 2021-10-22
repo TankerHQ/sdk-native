b0293994911b573cd07b835d7eb0c8580335dbe2
Philippe Daouadi <philippe.daouadi@tanker.io>
2021-10-22 17:34:59 +0200

    ProvisionalUserKeysStore: use new storage

--- modules/sdk-core/src/ProvisionalUsers/ProvisionalUserKeysStore.cpp
+++ modules/sdk-core/src/ProvisionalUsers/ProvisionalUserKeysStore.cpp
@@ -1,23 +1,97 @@
 #include <Tanker/ProvisionalUsers/ProvisionalUserKeysStore.hpp>
 
 #include <Tanker/Crypto/Format/Format.hpp>
-#include <Tanker/DataStore/Database.hpp>
 #include <Tanker/DataStore/Utils.hpp>
 #include <Tanker/DbModels/ProvisionalUserKeys.hpp>
+#include <Tanker/Encryptor/v2.hpp>
+#include <Tanker/Errors/Exception.hpp>
 #include <Tanker/Log/Log.hpp>
+#include <Tanker/Serialization/Serialization.hpp>
 #include <Tanker/Tracer/ScopeTimer.hpp>
 
 #include <sqlpp11/sqlite3/insert_or.h>
 
 TLOG_CATEGORY(ProvisionalUserKeysStore);
 
-using ProvisionalUserKeysTable =
-    Tanker::DbModels::provisional_user_keys::provisional_user_keys;
-
 namespace Tanker
 {
-ProvisionalUserKeysStore::ProvisionalUserKeysStore(DataStore::Database* dbConn)
-  : _db(dbConn)
+namespace
+{
+std::string const KeyPrefix = "provisionaluserkeys-";
+std::string const IndexPrefix = "provisionaluserkeys-index-";
+
+std::vector<uint8_t> serializeStoreKey(
+    Crypto::PublicSignatureKey const& appPublicSignatureKey,
+    Crypto::PublicSignatureKey const& tankerPublicSignatureKey)
+{
+  std::vector<uint8_t> keyBuffer(KeyPrefix.size() +
+                                 appPublicSignatureKey.size() +
+                                 tankerPublicSignatureKey.size());
+  auto it = keyBuffer.data();
+  it = std::copy(KeyPrefix.begin(), KeyPrefix.end(), it);
+  it = Serialization::serialize(it, appPublicSignatureKey);
+  it = Serialization::serialize(it, tankerPublicSignatureKey);
+  assert(it == keyBuffer.data() + keyBuffer.size());
+  return keyBuffer;
+}
+
+std::vector<uint8_t> serializeStoreValue(
+    ProvisionalUserKeys const& provisionalUserKeys)
+{
+  std::vector<uint8_t> valueBuffer(
+      provisionalUserKeys.appKeys.publicKey.size() +
+      provisionalUserKeys.appKeys.privateKey.size() +
+      provisionalUserKeys.tankerKeys.publicKey.size() +
+      provisionalUserKeys.tankerKeys.privateKey.size());
+  auto it = valueBuffer.data();
+  it = Serialization::serialize(it, provisionalUserKeys.appKeys.publicKey);
+  it = Serialization::serialize(it, provisionalUserKeys.appKeys.privateKey);
+  it = Serialization::serialize(it, provisionalUserKeys.tankerKeys.publicKey);
+  it = Serialization::serialize(it, provisionalUserKeys.tankerKeys.privateKey);
+  assert(it == valueBuffer.data() + valueBuffer.size());
+  return valueBuffer;
+}
+
+ProvisionalUserKeys deserializeStoreValue(gsl::span<uint8_t const> serialized)
+{
+  ProvisionalUserKeys provisionalUserKeys;
+  auto it = Serialization::SerializedSource{serialized};
+  Serialization::deserialize_to(it, provisionalUserKeys.appKeys.publicKey);
+  Serialization::deserialize_to(it, provisionalUserKeys.appKeys.privateKey);
+  Serialization::deserialize_to(it, provisionalUserKeys.tankerKeys.publicKey);
+  Serialization::deserialize_to(it, provisionalUserKeys.tankerKeys.privateKey);
+
+  if (!it.eof())
+    throw Errors::formatEx(
+        Errors::Errc::InternalError,
+        "failed to deserialize provisional user keys, the cache is corrupt");
+
+  return provisionalUserKeys;
+}
+
+std::vector<uint8_t> serializeIndexKey(
+    Crypto::PublicSignatureKey const& appPublicSignatureKey)
+{
+  std::vector<uint8_t> keyBuffer(IndexPrefix.size() +
+                                 appPublicSignatureKey.size());
+  auto it = keyBuffer.data();
+  it = std::copy(IndexPrefix.begin(), IndexPrefix.end(), it);
+  it = Serialization::serialize(it, appPublicSignatureKey);
+  assert(it == keyBuffer.data() + keyBuffer.size());
+  return keyBuffer;
+}
+
+std::vector<uint8_t> serializeIndexValue(
+    Crypto::PublicSignatureKey const& appPublicSignatureKey,
+    Crypto::PublicSignatureKey const& tankerPublicSignatureKey)
+{
+  return serializeStoreKey(appPublicSignatureKey, tankerPublicSignatureKey);
+}
+}
+
+ProvisionalUserKeysStore::ProvisionalUserKeysStore(
+    Crypto::SymmetricKey const& userSecret, DataStore::DataStore* db)
+  : _userSecret(userSecret), _db(db)
 {
 }
 
@@ -26,19 +100,27 @@ tc::cotask<void> ProvisionalUserKeysStore::putProvisionalUserKeys(
     Crypto::PublicSignatureKey const& tankerPublicSigKey,
     ProvisionalUserKeys const& provisionalUserKeys)
 {
+  FUNC_TIMER(DB);
   TINFO("Adding provisional user keys for {} {}",
         appPublicSigKey,
         tankerPublicSigKey);
-  FUNC_TIMER(DB);
-  ProvisionalUserKeysTable tab;
 
-  (*_db->connection())(sqlpp::sqlite3::insert_or_ignore_into(tab).set(
-      tab.app_pub_sig_key = appPublicSigKey.base(),
-      tab.tanker_pub_sig_key = tankerPublicSigKey.base(),
-      tab.app_enc_priv = provisionalUserKeys.appKeys.privateKey.base(),
-      tab.app_enc_pub = provisionalUserKeys.appKeys.publicKey.base(),
-      tab.tanker_enc_priv = provisionalUserKeys.tankerKeys.privateKey.base(),
-      tab.tanker_enc_pub = provisionalUserKeys.tankerKeys.publicKey.base()));
+  auto const keyBuffer = serializeStoreKey(appPublicSigKey, tankerPublicSigKey);
+  auto const valueBuffer = serializeStoreValue(provisionalUserKeys);
+
+  auto const indexKeyBuffer = serializeIndexKey(appPublicSigKey);
+  auto const indexValueBuffer =
+      serializeIndexValue(appPublicSigKey, tankerPublicSigKey);
+
+  std::vector<uint8_t> encryptedValue(
+      EncryptorV2::encryptedSize(valueBuffer.size()));
+  EncryptorV2::encryptSync(encryptedValue.data(), valueBuffer, _userSecret);
+
+  std::vector<std::pair<gsl::span<uint8_t const>, gsl::span<uint8_t const>>>
+      keyValues{{keyBuffer, encryptedValue},
+                {indexKeyBuffer, indexValueBuffer}};
+
+  _db->putCacheValues(keyValues, DataStore::OnConflict::Ignore);
   TC_RETURN();
 }
 
@@ -48,25 +130,28 @@ ProvisionalUserKeysStore::findProvisionalUserKeys(
     Crypto::PublicSignatureKey const& tankerPublicSigKey) const
 {
   FUNC_TIMER(DB);
-  ProvisionalUserKeysTable tab{};
-  auto rows = (*_db->connection())(
-      select(tab.app_enc_priv,
-             tab.app_enc_pub,
-             tab.tanker_enc_priv,
-             tab.tanker_enc_pub)
-          .from(tab)
-          .where(tab.app_pub_sig_key == appPublicSigKey.base() and
-                 tab.tanker_pub_sig_key == tankerPublicSigKey.base()));
-  if (rows.empty())
-    TC_RETURN(std::nullopt);
-  auto const& row = rows.front();
-  Tanker::ProvisionalUserKeys ret{
-      {DataStore::extractBlob<Crypto::PublicEncryptionKey>(row.app_enc_pub),
-       DataStore::extractBlob<Crypto::PrivateEncryptionKey>(row.app_enc_priv)},
-      {DataStore::extractBlob<Crypto::PublicEncryptionKey>(row.tanker_enc_pub),
-       DataStore::extractBlob<Crypto::PrivateEncryptionKey>(
-           row.tanker_enc_priv)}};
-  TC_RETURN(ret);
+
+  try
+  {
+    auto const keyBuffer =
+        serializeStoreKey(appPublicSigKey, tankerPublicSigKey);
+    auto const keys = {gsl::make_span(keyBuffer)};
+    auto const result = _db->findCacheValues(keys);
+    if (!result.at(0))
+      TC_RETURN(std::nullopt);
+
+    auto const& encryptedValue = *result.at(0);
+    std::vector<uint8_t> decryptedValue(
+        EncryptorV2::decryptedSize(encryptedValue));
+    TC_AWAIT(EncryptorV2::decrypt(
+        decryptedValue.data(), _userSecret, encryptedValue));
+
+    TC_RETURN(deserializeStoreValue(decryptedValue));
+  }
+  catch (Errors::Exception const& e)
+  {
+    DataStore::handleError(e);
+  }
 }
 
 tc::cotask<std::optional<Tanker::ProvisionalUserKeys>>
@@ -74,23 +159,32 @@ ProvisionalUserKeysStore::findProvisionalUserKeysByAppPublicSignatureKey(
     Crypto::PublicSignatureKey const& appPublicSignatureKey) const
 {
   FUNC_TIMER(DB);
-  ProvisionalUserKeysTable tab{};
-  auto rows = (*_db->connection())(
-      select(tab.app_enc_priv,
-             tab.app_enc_pub,
-             tab.tanker_enc_priv,
-             tab.tanker_enc_pub)
-          .from(tab)
-          .where(tab.app_pub_sig_key == appPublicSignatureKey.base()));
-  if (rows.empty())
-    TC_RETURN(std::nullopt);
-  auto const& row = rows.front();
-  Tanker::ProvisionalUserKeys ret{
-      {DataStore::extractBlob<Crypto::PublicEncryptionKey>(row.app_enc_pub),
-       DataStore::extractBlob<Crypto::PrivateEncryptionKey>(row.app_enc_priv)},
-      {DataStore::extractBlob<Crypto::PublicEncryptionKey>(row.tanker_enc_pub),
-       DataStore::extractBlob<Crypto::PrivateEncryptionKey>(
-           row.tanker_enc_priv)}};
-  TC_RETURN(ret);
+
+  try
+  {
+    auto const indexKey = serializeIndexKey(appPublicSignatureKey);
+    auto const indexKeys = {gsl::make_span(indexKey)};
+    auto const indexResult = _db->findCacheValues(indexKeys);
+    if (!indexResult.at(0))
+      TC_RETURN(std::nullopt);
+
+    auto const keys = {gsl::make_span(*indexResult.at(0))};
+    auto const result = _db->findCacheValues(keys);
+    if (!result.at(0))
+      // There's an index but no entry, weird...
+      TC_RETURN(std::nullopt);
+
+    auto const& encryptedValue = *result.at(0);
+    std::vector<uint8_t> decryptedValue(
+        EncryptorV2::decryptedSize(encryptedValue));
+    TC_AWAIT(EncryptorV2::decrypt(
+        decryptedValue.data(), _userSecret, encryptedValue));
+
+    TC_RETURN(deserializeStoreValue(decryptedValue));
+  }
+  catch (Errors::Exception const& e)
+  {
+    DataStore::handleError(e);
+  }
 }
 }
