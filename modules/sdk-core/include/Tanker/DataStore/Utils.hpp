4b34f16f1df8a907b015ed606bb1219714477373
Philippe Daouadi <philippe.daouadi@tanker.io>
2021-11-02 14:20:50 +0100

    DataStore: add error handling helper

--- modules/sdk-core/include/Tanker/DataStore/Utils.hpp
+++ modules/sdk-core/include/Tanker/DataStore/Utils.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <Tanker/Errors/Exception.hpp>
+
 #include <cstdint>
 
 #include <gsl/gsl-lite.hpp>
@@ -13,5 +15,7 @@ T extractBlob(Field const& f)
 {
   return T(f.blob, f.blob + f.len);
 }
+
+[[noreturn]] void handleError(Errors::Exception const& e);
 }
 }
