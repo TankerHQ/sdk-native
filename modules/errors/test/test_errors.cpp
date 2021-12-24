12a12b6b297bda251f714c68e461c6df8a837e30
Philippe Daouadi <philippe.daouadi@tanker.io>
2021-12-24 13:58:07 +0100

    errors: migrate tests to catch2

--- modules/errors/test/test_errors.cpp
+++ modules/errors/test/test_errors.cpp
@@ -1,23 +1,20 @@
 #include <Tanker/Errors/Errc.hpp>
 
-#include <doctest/doctest.h>
-// VERY IMPORTANT to include this:
-// https://github.com/onqtam/doctest/issues/183
-#include <ostream>
+#include <catch2/catch.hpp>
 
 using namespace Tanker::Errors;
 
 TEST_CASE("Errors")
 {
-  SUBCASE("Errc")
+  SECTION("Errc")
   {
     {
       std::error_condition ec = Errc::InvalidArgument;
-      CHECK_EQ(ec, Errc::InvalidArgument);
+      CHECK(ec == Errc::InvalidArgument);
     }
     {
       auto const ec = make_error_code(Errc::OperationCanceled);
-      CHECK_EQ(ec, Errc::OperationCanceled);
+      CHECK(ec == Errc::OperationCanceled);
     }
   }
 }
