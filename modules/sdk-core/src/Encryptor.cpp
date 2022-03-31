5d6c370a211a74c603814026d3669639a753e8cb
Philippe Daouadi <philippe.daouadi@doctolib.com>
2022-03-31 09:58:43 +0200

    feat(Encryptor): encrypt huge data with padded streams
    Include potential padding when choosing the encryption format.
    
    This way, if you encrypt 2B with 200MB of padding, we will effectively
    choose the stream format. If we didn't, it would leak the info that the
    clear buffer was actually < 1MB.

--- modules/sdk-core/src/Encryptor.cpp
+++ modules/sdk-core/src/Encryptor.cpp
@@ -51,24 +51,28 @@ decltype(auto) performEncryptorAction(std::uint32_t version, Callable&& cb)
 }
 }
 
-bool isHugeClearData(uint64_t dataSize)
+bool isHugeClearData(uint64_t dataSize, std::optional<uint32_t> paddingStep)
 {
-  return dataSize > Streams::Header::defaultEncryptedChunkSize;
+  return Padding::paddedFromClearSize(dataSize, paddingStep) >
+         Streams::Header::defaultEncryptedChunkSize;
 }
 
 uint64_t encryptedSize(uint64_t clearSize, std::optional<uint32_t> paddingStep)
 {
-  if (isHugeClearData(clearSize))
+  if (isHugeClearData(clearSize, paddingStep))
   {
-    // For now, there is no padded stream encryptor so we ignore the paddingStep
-    // argument in case of an automatic redirection
-    return EncryptorV4::encryptedSize(clearSize);
+    if (paddingStep == Padding::Off)
+      return EncryptorV4::encryptedSize(clearSize);
+    else
+      return EncryptorV8::encryptedSize(clearSize, paddingStep);
   }
-
-  if (paddingStep == Padding::Off)
-    return EncryptorV3::encryptedSize(clearSize);
-
-  return EncryptorV6::encryptedSize(clearSize, paddingStep);
+  else
+  {
+    if (paddingStep == Padding::Off)
+      return EncryptorV3::encryptedSize(clearSize);
+    else
+      return EncryptorV6::encryptedSize(clearSize, paddingStep);
+  }
 }
 
 uint64_t decryptedSize(gsl::span<uint8_t const> encryptedData)
@@ -88,18 +92,22 @@ tc::cotask<EncryptionMetadata> encrypt(gsl::span<uint8_t> encryptedData,
                                        gsl::span<uint8_t const> clearData,
                                        std::optional<uint32_t> paddingStep)
 {
-  if (isHugeClearData(clearData.size()))
+  if (isHugeClearData(clearData.size(), paddingStep))
   {
-    // For now, there is no padded stream encryptor so we ignore the paddingStep
-    // argument in case of an automatic redirection
-    TC_RETURN(TC_AWAIT(EncryptorV4::encrypt(encryptedData, clearData)));
+    if (paddingStep == Padding::Off)
+      TC_RETURN(TC_AWAIT(EncryptorV4::encrypt(encryptedData, clearData)));
+    else
+      TC_RETURN(TC_AWAIT(
+          EncryptorV8::encrypt(encryptedData, clearData, paddingStep)));
   }
-
-  if (paddingStep == Padding::Off)
-    TC_RETURN(TC_AWAIT(EncryptorV3::encrypt(encryptedData, clearData)));
-
-  TC_RETURN(
-      TC_AWAIT(EncryptorV6::encrypt(encryptedData, clearData, paddingStep)));
+  else
+  {
+    if (paddingStep == Padding::Off)
+      TC_RETURN(TC_AWAIT(EncryptorV3::encrypt(encryptedData, clearData)));
+    else
+      TC_RETURN(TC_AWAIT(
+          EncryptorV6::encrypt(encryptedData, clearData, paddingStep)));
+  }
 }
 
 tc::cotask<uint64_t> decrypt(gsl::span<uint8_t> decryptedData,
