fe2fc0b8d1fe2cf24f88b1dd0f66521a1141c585
Timothée Isnard <timothee.isnard@tanker.io>
2022-10-10 02:42:10 +0200

    feat: CompositeResourceId and SimpleResourceId types
    With transparent sessions, the resource ID can now be a composite
    of the individual resource's ID (used to share just the resource)
    and the session ID, and thus can refer to multiple keys.
    
    The simple resource ID is the same as the old resource ID type,
    and only identifies to a single (resource or session) key.

---
+++ modules/crypto/include/Tanker/Crypto/CompositeResourceId.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <Tanker/Crypto/BasicCryptographicType.hpp>
+#include <Tanker/Crypto/Mac.hpp>
+#include <Tanker/Crypto/SimpleResourceId.hpp>
+
+#include <gsl/gsl-lite.hpp>
+
+namespace Tanker::Crypto
+{
+namespace details
+{
+constexpr auto compositeTypeSize = 1;
+constexpr auto compositeSize = compositeTypeSize + SimpleResourceId::arraySize +
+                               SimpleResourceId::arraySize;
+}
+
+// Currently not a variant as there is still only a single composite resource
+// ID type (type 0, used by transparent sessions)
+// Format: Type (1 Byte) | Session ID (16 B) | Individual resource ID (16 B) |
+class CompositeResourceId
+  : public BasicCryptographicType<CompositeResourceId, details::compositeSize>
+{
+  using base_t::base_t;
+
+public:
+  // For now, this is the only composite ID type
+  static constexpr uint8_t transparentSessionType()
+  {
+    return 0;
+  }
+
+  uint8_t type() const
+  {
+    return *begin();
+  }
+
+  SimpleResourceId sessionId() const
+  {
+    auto first = begin() + details::compositeTypeSize;
+    auto last = first + SimpleResourceId::arraySize;
+    return SimpleResourceId(first, last);
+  }
+
+  SimpleResourceId individualResourceId() const
+  {
+    auto first =
+        begin() + details::compositeTypeSize + SimpleResourceId::arraySize;
+    auto last = first + SimpleResourceId::arraySize;
+    return SimpleResourceId(first, last);
+  };
+};
+}
