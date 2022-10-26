54fd54d805676965bec21ec2a34ca6dc0d1c79b6
Timothée Isnard <timothee.isnard@tanker.io>
2022-10-26 16:29:00 +0200

    fix: Link new tankerencryptor module with C bindings

--- conanfile.py
+++ conanfile.py
@@ -179,6 +179,7 @@ class TankerConan(ConanFile):
                     "tankeradmin",
                     "tankertesthelpers",
                     "tankercore",
+                    "tankerencryptor",
                     "tankerstreams",
                     "tankertrustchain",
                     "tankeridentity",
