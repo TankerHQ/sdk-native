2a11ef92ba7b26fb9efc42da296becfbd656e56a
julien mounier <julien.mounier@doctolib.com>
2023-02-02 17:38:36 +0100

    fix(reporter): list test sections as well as test cases

--- modules/functional-tests/TestNameReporter.hpp
+++ modules/functional-tests/TestNameReporter.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
-#include <catch2/reporters/catch_reporter_streaming_base.hpp>
+#include <boost/algorithm/string/join.hpp>
+
+#include <catch2/reporters/catch_reporter_cumulative_base.hpp>
 #include <catch2/catch_test_case_info.hpp>
 #include <catch2/reporters/catch_reporter_registrars.hpp>
 
@@ -8,22 +10,43 @@
 #include <range/v3/range/conversion.hpp>
 #include <range/v3/view/transform.hpp>
 
-class TestNameReporter : public Catch::StreamingReporterBase {
+class TestNameReporter : public Catch::CumulativeReporterBase {
 public:
-    using StreamingReporterBase::StreamingReporterBase;
+    using CumulativeReporterBase::CumulativeReporterBase;
 
     static std::string getDescription() {
         return "Reporter for formating test names in a JSON array";
     }
 
-    void listTests(std::vector<Catch::TestCaseHandle> const& tests) override {
-        auto testNames = tests |
-             ranges::views::transform(&Catch::TestCaseHandle::getTestCaseInfo) |
-             ranges::views::transform(&Catch::TestCaseInfo::name) |
-             ranges::to<std::vector>;
+    void testCaseStarting(Catch::TestCaseInfo const& testInfo) override {
+        CumulativeReporterBase::testCaseStarting(testInfo);
 
-        m_stream << nlohmann::json(testNames).dump();
+        _testStack.emplace_back(testInfo.name);
+    };
+
+    void testCaseEnded(Catch::TestCaseStats const& testCaseStats) override {
+        CumulativeReporterBase::testCaseEnded(testCaseStats);
+
+        if (!_testStack.empty()) {
+            _testStack.pop_back();
+        }
     }
+
+    void sectionStarting(Catch::SectionInfo const& sectionInfo) override {
+        CumulativeReporterBase::sectionStarting(sectionInfo);
+
+        _testStack.emplace_back(sectionInfo.name);
+        _testNames.emplace_back(boost::algorithm::join(_testStack, " "));
+        _testStack.pop_back();
+    }
+
+    void testRunEndedCumulative() override {
+        m_stream << nlohmann::json(_testNames).dump();
+    };
+
+private:
+    std::vector<std::string> _testStack{};
+    std::vector<std::string> _testNames{};
 };
 
 CATCH_REGISTER_REPORTER("test-names", TestNameReporter);
