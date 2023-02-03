119ba23a28a0ceaadec47090b80bee9c21974d00
julien mounier <julien.mounier@doctolib.com>
2023-02-03 15:01:52 +0100

    test(reporter): report leaf sections only
    SECTIONs can be used inside one another and TEST_CASE always have
    an implicite SECTION containing the whole TEST_CASE content
    (aka. sub SECTIONs)
    
    This fix prevent outter and intermediate SECTIONs (ie. TEST_CASE)
    to be reported multiple times

--- modules/functional-tests/TestNameReporter.hpp
+++ modules/functional-tests/TestNameReporter.hpp
@@ -1,52 +1,83 @@
 #pragma once
 
-#include <boost/algorithm/string/join.hpp>
-
 #include <catch2/reporters/catch_reporter_cumulative_base.hpp>
 #include <catch2/catch_test_case_info.hpp>
 #include <catch2/reporters/catch_reporter_registrars.hpp>
 
+#include <Tanker/Log/LogHandler.hpp>
+
+#include <boost/algorithm/string.hpp>
+#include <fmt/format.h>
+
 #include <nlohmann/json.hpp>
-#include <range/v3/range/conversion.hpp>
-#include <range/v3/view/transform.hpp>
+
+namespace
+{
+struct Test {
+  std::string name;
+  bool isLeaf;
+
+  Test(std::string const& testName)
+    : name{testName},
+      isLeaf{true} {
+    boost::trim_right(name);
+  }
+};
+
+void silentLogHandler(Tanker::Log::Record const&) {}
+}
 
 class TestNameReporter : public Catch::CumulativeReporterBase {
 public:
-    using CumulativeReporterBase::CumulativeReporterBase;
+  using CumulativeReporterBase::CumulativeReporterBase;
 
-    static std::string getDescription() {
-        return "Reporter for formating test names in a JSON array";
+  static std::string getDescription() {
+    return "Reporter for formating test names in a JSON array";
+  }
+
+  void testRunStarting(Catch::TestRunInfo const& testRunInfo) override {
+    CumulativeReporterBase::testRunStarting(testRunInfo);
+
+    Tanker::Log::setLogHandler(&silentLogHandler);
+  }
+
+  void sectionStarting(Catch::SectionInfo const& sectionInfo) override {
+    CumulativeReporterBase::sectionStarting(sectionInfo);
+
+    stackTest(sectionInfo.name);
+  }
+
+  void sectionEnded(Catch::SectionStats const& sectionStats) override {
+    CumulativeReporterBase::sectionEnded(sectionStats);
+
+    auto const currentTest = _testStack.back();
+    if (currentTest.isLeaf) {
+      _testNames.emplace_back(currentTest.name);
     }
 
-    void testCaseStarting(Catch::TestCaseInfo const& testInfo) override {
-        CumulativeReporterBase::testCaseStarting(testInfo);
+    _testStack.pop_back();
+  }
 
-        _testStack.emplace_back(testInfo.name);
-    };
-
-    void testCaseEnded(Catch::TestCaseStats const& testCaseStats) override {
-        CumulativeReporterBase::testCaseEnded(testCaseStats);
-
-        if (!_testStack.empty()) {
-            _testStack.pop_back();
-        }
-    }
-
-    void sectionStarting(Catch::SectionInfo const& sectionInfo) override {
-        CumulativeReporterBase::sectionStarting(sectionInfo);
-
-        _testStack.emplace_back(sectionInfo.name);
-        _testNames.emplace_back(boost::algorithm::join(_testStack, " "));
-        _testStack.pop_back();
-    }
-
-    void testRunEndedCumulative() override {
-        m_stream << nlohmann::json(_testNames).dump();
-    };
+  void testRunEndedCumulative() override {
+    m_stream << nlohmann::json(_testNames).dump(2);
+  };
 
 private:
-    std::vector<std::string> _testStack{};
-    std::vector<std::string> _testNames{};
+  void stackTest(std::string sectionName) {
+
+    if (_testStack.empty()) {
+      _testStack.emplace_back(sectionName);
+      return;
+    }
+
+    auto& lastSection = _testStack.back();
+    lastSection.isLeaf = false;
+
+    _testStack.emplace_back(fmt::format("{:s} {:s}", lastSection.name, sectionName));
+  };
+
+  std::vector<Test> _testStack{};
+  std::vector<std::string> _testNames{};
 };
 
 CATCH_REGISTER_REPORTER("test-names", TestNameReporter);
