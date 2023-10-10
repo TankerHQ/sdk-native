b663312f8f86c4896ca157b93661665363bdfab7
Timothée Isnard <timothee.isnard@tanker.io>
2023-10-10 17:44:22 +0200

    maint: Update range-v3

--- conanfile.py
+++ conanfile.py
@@ -98,7 +98,7 @@ class TankerConan(ConanFile):
             self.requires("sqlpp11-connector-sqlite3/0.30-r7", private=private)
         self.requires("mgs/0.2.1-r1", private=private)
         self.requires("enum-flags/0.1a-r3", private=private)
-        self.requires("range-v3/0.11.0-r4", private=private)
+        self.requires("range-v3/0.12.0-r1", private=private)
         self.requires("fmt/7.1.3-r3", private=private)
         self.requires("gsl-lite/0.37.0-r1", private=private)
         self.requires("nlohmann_json/3.10.5-r1", private=private)
